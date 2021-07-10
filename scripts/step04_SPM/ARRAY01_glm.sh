70c3e3df76923c0e58460e120d69083eb911af96
jungheejung <heejung.jung@colorado.edu>
2021-07-10 16:37:17 -0400

    skips missing onset or missing nifti runs

--- scripts/step04_SPM/ARRAY01_glm.sh
+++ scripts/step04_SPM/ARRAY01_glm.sh
@@ -3,13 +3,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=10:00:00
-#SBATCH -o ./log/preproc_%A_%a.o
-#SBATCH -e ./log/preproc_%A_%a.e
+#SBATCH --time=12:00:00
+#SBATCH -o ./log_glm/glm_%A_%a.o
+#SBATCH -e ./log_glm/glm_%A_%a.e
 #SBATCH --account=DBIC
-#SBATCH --partition=standard
-#SBATCH --array=3-15%7
-## --array=1-17%5
+#SBATCH --partition=preemptable
+#SBATCH --array=6
+#1-17%5
 
 
 subjects=( "'0002'" "'0003'" "'0004'" "'0005'" "'0006'" "'0007'" "'0008'" "'0009'" "'0010'" \
@@ -22,6 +22,8 @@ SCRIPT_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts
 SUBJECT=${SLURM_ARRAY_TASK_ID//[!0-9]/}
 echo ${SUBJECT}
 # sub_list = {2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,23,24,25}
-module load matlab/r2020a
-matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); glm_discovery_job($PARTICIPANT_LABEL);"
 
+module load matlab/r2020a
+matlab -nodesktop -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); glm($PARTICIPANT_LABEL);"
+
+echo "matlab -nodesktop -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); glm($PARTICIPANT_LABEL);"
