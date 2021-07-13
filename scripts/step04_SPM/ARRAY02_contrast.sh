b67a2c8c4eae796eb38514a59e797fc263cc3599
jungheejung <heejung.jung@colorado.edu>
2021-07-12 20:30:05 -0400

    contrast managre

--- scripts/step04_SPM/ARRAY02_contrast.sh
+++ scripts/step04_SPM/ARRAY02_contrast.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=01:00:00
-#SBATCH -o ./log/preproc_%A_%a.o
-#SBATCH -e ./log/preproc_%A_%a.e
+#SBATCH --time=00:30:00
+#SBATCH -o ./log_con/contrast_%A_%a.o
+#SBATCH -e ./log_con/contrast_%A_%a.e
 #SBATCH --account=DBIC
-#SBATCH --partition=standard
+#SBATCH --partition=preemptable
 #SBATCH --array=0-15%7
 ## --array=1-17%5
 
@@ -23,5 +23,5 @@ SUBJECT=${SLURM_ARRAY_TASK_ID//[!0-9]/}
 echo ${SUBJECT}
 # sub_list = {2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,23,24,25}
 module load matlab/r2020a
-matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); first_level_contrast($PARTICIPANT_LABEL);"
+matlab -nodisplay -nosplash -batch "addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); firstlevel02_contrast($PARTICIPANT_LABEL);"
 
