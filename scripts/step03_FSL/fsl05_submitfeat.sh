0a08dfec8ed26c7cc5d24a14845783567f2017d2
jungheejung <heejung.jung@colorado.edu>
2021-08-19 22:03:59 -0400

    update parameters

--- scripts/step03_FSL/fsl05_submitfeat.sh
+++ scripts/step03_FSL/fsl05_submitfeat.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -l
 #SBATCH --job-name=fsl
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1 
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=06:00:00
+#SBATCH --time=03:00:00
 #SBATCH -o ./log_fsl/FSL_%A_%a.o
 #SBATCH -e ./log_fsl/FSL_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-4%4
+#SBATCH --array=601-700%20
 
 module load fsl/6.0.4
+conda activate spacetop_env
 
 FMRIPREP_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/derivatives/dartmouth/fmriprep/fmriprep"
 EV_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/analysis/fmri/fsl/multivariate/isolate_ev"
@@ -34,21 +36,25 @@ if [ -z "$CHECK" ];
     NII=${FMRIPREP_DIR}/${SUB}/${SES}/func/${SUB}_${SES}_task-social_acq-mb8_run-${RUN_NUM}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii
 
     if test -f "${BOLD}"; then
-        echo "brain data exists."
+        echo "INFO: ${SUB},${SES},${RUN},${EV}"
+        echo "STEP01: brain data exists. PROCEED with model"
         # TODO: delete .nii; otherwise, FSL will error and say there are redundant basenames
         if test -f "${NII}"; then 
+	    echo "STEP02: deleting .nii..."
             rm -rf ${NII}
         fi
         cd ${EV_DIR}/${SUB}/${SES}/${RUN}/${EV}
+        echo "STEP03: starting FEAT"
         feat isolated_model.fsf
     fi
     echo "JOB COMPLETE: ${SUB},${SES},${RUN},${EV}"
     # TODO: 4. Append DONE to the textfiles once jobs complete. 
     awk -v n=${IND} -v string=',DONE' 'NR==n { $0 = $0 string} 1' ${ARRAY_FILE}  > tmp && mv tmp ${ARRAY_FILE}
+    echo "STEP04: updating list - mark as complete"
     
 else 
     if [ "$CHECK" = "DONE" ]; then
-    "ALREADY COMPLETE"
+    echo "ALREADY COMPLETE"
     exit
     fi
 fi
