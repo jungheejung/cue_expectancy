9120fe42c727f64e4bdafe710503cb91e76adea9
jungheejung <heejung.jung@colorado.edu>
2021-10-06 02:39:50 -0400

    mediation after concatenating niftis

---
+++ scripts/step05_mediation/ARRAY02_mediation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+#SBATCH --job-name=glm
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=12:00:00
+#SBATCH -o ./log/M_%A_%a.o
+#SBATCH -e ./log/M_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+
+
+# echo "array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
+CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
+SCRIPT_DIR=${PWD}
+
+module load matlab/r2020a
+matlab -nodesktop -nosplash -batch "opengl('save','hardware'); 
+rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; 
+rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MediationToolbox'));
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore'));
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'));
+rmpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12/external/fieldtrip/external/stats');
+addpath(genpath(${SCRIPT_DIR})); 
+multi02_mediation"
+
+echo "matlab -nodesktop -nosplash -batch "opengl('save','hardware'); 
+rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; 
+rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MediationToolbox'));
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore'));
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'));
+rmpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12/external/fieldtrip/external/stats');
+addpath(genpath(${SCRIPT_DIR})); 
+multi02_mediation"
