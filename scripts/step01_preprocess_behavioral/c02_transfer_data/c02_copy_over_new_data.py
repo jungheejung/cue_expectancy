5ef4fba371e7d1c3bf14cf467800c67b91abbde5
jungheejung <>
2021-10-12 00:59:47 -0400

    update path

--- scripts/step01_preprocess_behavioral/c02_transfer_data/c02_copy_over_new_data.py
+++ scripts/step01_preprocess_behavioral/c02_transfer_data/c02_copy_over_new_data.py
@@ -13,7 +13,7 @@ data_repo_dir = "/Users/h/Documents/projects_local/d_beh" # USER, INSERT PATH
 """
 search for folder named d_beh
 copy /Users/h/Documents/projects_local/d_beh/sub-0001/task-social
-into /Users/h/Documents/projects_local/social_influence_analysis/data/dartmouth/d01_beh_raw
+into /Users/h/Documents/projects_local/social_influence_analysis/data/dartmouth/d01_rawbeh
 """
 
 
@@ -22,7 +22,7 @@ for ind, src_fname in enumerate(src):
 
     num = re.findall('\d+', src[ind])
     print("sub: {0}, session: {1}".format(num[0], num[1]))
-    dst_fname = os.path.join( analysis_repo_dir, 'data', 'dartmouth', 'd01_beh_raw', 'sub-'+num[0], 'ses-'+num[1])
+    dst_fname = os.path.join( analysis_repo_dir, 'data', 'dartmouth', 'd01_rawbeh', 'sub-'+num[0], 'ses-'+num[1])
     if os.path.exists(dst_fname):
         print("folder exists")
     else:
