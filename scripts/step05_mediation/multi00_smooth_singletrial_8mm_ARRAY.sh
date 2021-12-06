cc606590c388a372a72992163eea96696f24cc8e
jungheejung <heejung.jung@colorado.edu>
2021-12-06 04:31:37 -0500

    update path and subject number

--- scripts/step05_mediation/multi00_smooth_singletrial_8mm_ARRAY.sh
+++ scripts/step05_mediation/multi00_smooth_singletrial_8mm_ARRAY.sh
@@ -12,23 +12,18 @@
 #1-17%5
 
 
-# subjects=( "'0002'" "'0003'" "'0004'" "'0005'" "'0006'" "'0007'" "'0008'" "'0009'" "'0010'" "'0011'" \
-# "'0013'" "'0014'" "'0015'" "'0016'" "'0017'" "'0018'"  "'0019'" "'0020'" "'0021'" "'0023'" \
-# "'0024'" "'0025'" "'0026'" "'0028'" "'0029'")
-
-subjects=("'sub-0030'" "'sub-0031'" "'sub-0032'" "'sub-0033'" "'sub-0035'" \
+subjects=( "'0002'" "'0003'" "'0004'" "'0005'" "'0006'" "'0007'" "'0008'" "'0009'" "'0010'" "'0011'" \
+"'0013'" "'0014'" "'0015'" "'0016'" "'0017'" "'0018'"  "'0019'" "'0020'" "'0021'" "'0023'" \
+"'0024'" "'0025'" "'0026'" "'0028'" "'0029'" "'sub-0030'" "'sub-0031'" "'sub-0032'" "'sub-0033'" "'sub-0035'" \
 "'sub-0037'" "'sub-0043'" "'sub-0047'" "'sub-0051'" "'sub-0053'" "'sub-0055'" \
 "'sub-0058'" "'sub-0060'")
-# "'0011'" "'0013'" "'0016'" "'0017'" "'0018'" "'0019'" "'0020'" "'0024'"
-#subjects=(2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,20)
 PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID -1 ))]}
 echo "array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
 CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
 SCRIPT_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM"
 SUBJECT=${SLURM_ARRAY_TASK_ID//[!0-9]/}
 echo ${PARTICIPANT_LABEL}
-# sub_list = {2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,23,24,25}
 module load matlab/r2020a
-# matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); s00_smooth($PARTICIPANT_LABEL);"
+#matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); s00_smooth($PARTICIPANT_LABEL);"
 
-matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step05_mediation')); s00_smooth_8mm($PARTICIPANT_LABEL);"
+matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step05_mediation')); multi00_smooth_singletrial_8mm($PARTICIPANT_LABEL);"
