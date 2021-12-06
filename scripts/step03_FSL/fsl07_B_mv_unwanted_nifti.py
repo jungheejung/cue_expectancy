697218c9efeae5c85020acbfefd941719776895f
jungheejung <heejung.jung@colorado.edu>
2021-12-05 22:00:26 -0500

    enable remove file

--- scripts/step03_FSL/fsl07_B_mv_unwanted_nifti.py
+++ scripts/step03_FSL/fsl07_B_mv_unwanted_nifti.py
@@ -18,9 +18,9 @@ import pdb
 from pathlib import Path
 import itertools
 
-sub_num = sys.argv[1]
-ses = sys.argv[2]
-run = sys.argv[3]
+sub_num = int(float(sys.argv[1]))
+ses = int(float(sys.argv[2]))
+run = int(float(sys.argv[3]))
 print(f"subject: {sub_num}, ses: {ses}, run: {run}, nifti")
 # %% parameters ________________________________________________________________________
 current_dir = os.getcwd()
@@ -28,7 +28,7 @@ main_dir = Path(current_dir).parents[1] # discovery: /dartfs-hpc/rc/lab/C/CANlab
 print(main_dir)
 print(sub_num)
 print(type(sub_num))
-sub_str = f"sub-{int(sub_num):04d}"
+sub_str = f"sub-{sub_num:04d}"
 sub_dir = os.path.join(main_dir, 'analysis', 'fmri', 'fsl', 'multivariate', 'isolate_nifti', sub_str)
 print(sub_dir)
 Path(os.path.join(sub_dir, 'exclude')).mkdir(parents=True, exist_ok=True)
@@ -36,9 +36,9 @@ Path(os.path.join(sub_dir, 'exclude')).mkdir(parents=True, exist_ok=True)
 # %%
 key_input = {"ses": ses, "run": run}
 
-for i in range(len(key_input['run'])):
-    print(f"sub-{int(sub_num):04d}_ses-{int(key_input['ses']):02d}_run-{int(key_input['run'][i]):02d}*.nii.gz")
-    exclude_files = glob.glob(os.path.join(sub_dir, f"sub-{int(sub_num):04d}_ses-{int(key_input['ses']):02d}_run-{int(key_input['run'][i]):02d}*.nii.gz"))
+#vfor i in range(len(key_input['run'])):
+print(f"sub-{sub_num:04d}_ses-{key_input['ses']:02d}_run-{key_input['run']:02d}*.nii.gz")
+exclude_files = glob.glob(os.path.join(sub_dir, f"sub-{sub_num:04d}_ses-{key_input['ses']:02d}_run-{key_input['run']:02d}*.nii.gz"))
 print(exclude_files)
 for e_file in exclude_files:
     shutil.move(os.path.join(sub_dir, e_file), os.path.join(sub_dir, 'exclude'))
