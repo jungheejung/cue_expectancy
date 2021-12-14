4544b0eead288e9065237f7bfb80e58055b72c6b
jungheejung <heejung.jung@colorado.edu>
2021-12-13 22:22:52 -0500

    exclude niftis

---
+++ scripts/step06_SPMsingletrial/s03_exclude_nifti.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+remove contaminated nifti files
+create new sub folder, move select niftifiles to sub folder
+__author__ = "Heejung Jung"
+__copyright__ = "Copyright 2021, The Cogent Project"
+__credits__ = ["Heejung Jung"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Heejung Jung"
+__email__ = "heejung.jung@colorado.edu"
+__status__ = "Production"
+"""
+
+# %% libraries ________________________________________________________________________
+import os, sys, glob, shutil
+import pdb
+from pathlib import Path
+import itertools
+
+sub_num = int(float(sys.argv[1]))
+ses = int(float(sys.argv[2]))
+run = int(float(sys.argv[3]))
+print(f"subject: {sub_num}, ses: {ses}, run: {run}, nifti")
+# %% parameters ________________________________________________________________________
+current_dir = os.getcwd()
+main_dir = Path(current_dir).parents[1] # discovery: /dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social
+print(main_dir)
+print(sub_num)
+print(type(sub_num))
+sub_str = f"sub-{sub_num:04d}"
+sub_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's02_isolatenifti', sub_str)
+print(sub_dir)
+Path(os.path.join(sub_dir, 'exclude')).mkdir(parents=True, exist_ok=True)
+
+key_input = {"ses": ses, "run": run}
+
+print(f"sub-{sub_num:04d}_ses-{key_input['ses']:02d}_run-{key_input['run']:02d}*.nii.gz")
+exclude_files = glob.glob(os.path.join(sub_dir, f"sub-{sub_num:04d}_ses-{key_input['ses']:02d}_run-{key_input['run']:02d}*.nii.gz"))
+print(exclude_files)
+for e_file in exclude_files:
+    shutil.move(os.path.join(sub_dir, e_file), os.path.join(sub_dir, 'exclude'))
---
+++ scripts/step06_SPMsingletrial/s03_exclude_nifti_ARRAY.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#SBATCH --job-name=exclude
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=5gb
+#SBATCH --time=01:00:00
+#SBATCH -o ./log/exclude_%A_%a.o
+#SBATCH -e ./log/exclude_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=1-40
+
+source /optnfs/common/miniconda3/etc/profile.d/conda.sh
+conda activate spacetop_env
+
+MAINDIR=/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial
+
+
+echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
+IND=$((SLURM_ARRAY_TASK_ID+1))
+INFILE=`awk -F '\r\t\n' "NR==${SLURM_ARRAY_TASK_ID}" ./s03_exclude_list.txt`
+echo $INFILE
+SUB=$(echo $INFILE | cut -d$' ' -f1)
+SES=$(echo $INFILE | cut -d$' ' -f2)
+RUN=$(echo $INFILE | cut -d$' ' -f3)
+
+hostname -s
+python ./s03_exclude_nifti.py ${SUB} ${SES} ${RUN}
