b0bf9fee5088b9e93b6c6f63d36e430b65241c30
jungheejung <>
2021-12-15 11:03:44 +0900

    FIX: general tasks

--- scripts/step06_SPMsingletrial/s05_metadata.py
+++ scripts/step06_SPMsingletrial/s05_metadata.py
@@ -9,14 +9,14 @@ current_dir = os.getcwd()
 main_dir = Path(current_dir).parents[1] # discovery: /dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social
 nifti_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's03_concatnifti')
 sub_list = next(os.walk(nifti_dir))[1]
+items_to_remove = ['sub-0000', 'sub-0002']
+for item in items_to_remove:
+    if item in sub_list:
+        sub_list.remove(item)
 
 param_list = [sub_list, 
-['pain', 'vicarious', 'cognitive', 'general'],
+['pain', 'vicarious', 'cognitive'],
 ['cue', 'stim']]
-# task_list = []
-# ev_list = []
-# task = 'cognitive'
-# ev = 'cue'
 
 full_list = list(itertools.product(*param_list))
 for sub, task, ev in full_list:
@@ -34,8 +34,34 @@ for sub, task, ev in full_list:
     filtered = subset[subset.nifti_name  == nifti.fname]
     filtered.rename(columns={0:"beta_index_fsl"})
     save_fname = os.path.join(nifti_dir, sub, f"metadata_{sub}_task-{task}_ev-{ev}.csv")
-    try:
-        os.ulink(save_fname)
-    except:
-        print("Error while deleting file or file doesn't exist")   
+    if os.path.exists(save_fname):
+        os.remove(save_fname)
+    else:
+        print(f"{sub}_task-{task}_ev-{ev} doesnt exist")
     filtered.to_csv(save_fname)
+
+# general
+param_list = [sub_list, 
+['cue', 'stim']]
+
+full_list = list(itertools.product(*param_list))
+for sub, ev in full_list:
+# load niftifname txt 
+    nifti_fname = os.path.join(nifti_dir, sub, f"niftifname_{sub}_task-general_ev-{ev}.txt")
+    nifti = pd.read_csv(nifti_fname, sep = '\t', header = None)
+    # load f'{sub}_singletrial.csv
+    subject_csv = os.path.join(main_dir, 'data', 'dartmouth', 'd06_singletrial_SPM', sub, f"{sub}_singletrial.csv" )
+    meta = pd.read_csv(subject_csv)
+    # subset task and ev, # based on text file, # only grab rows that exist
+    nifti['fname'] = nifti[0].map(lambda x: x.lstrip('./').rstrip('.nii'))
+    subset = meta[(meta.ev == ev)].reset_index(drop = True)
+
+    # %%
+    filtered = subset[subset.nifti_name  == nifti.fname]
+    filtered.rename(columns={0:"beta_index_fsl"})
+    save_genfname = os.path.join(nifti_dir, sub, f"metadata_{sub}_task-general_ev-{ev}.csv")
+    if os.path.exists(save_genfname):
+        os.remove(save_genfname)
+    else:
+        print(f"{sub}_task-general_ev-{ev} doesnt exist")
+    filtered.to_csv(save_genfname)
