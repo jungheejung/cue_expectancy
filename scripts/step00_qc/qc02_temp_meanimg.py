e6550a26610d5b13b3eb4b4c7569810de75009d8
jungheejung <>
2022-01-13 12:48:46 -0500

    ENHANCE: overwrites existing pandas after check-up

--- scripts/step00_qc/qc02_temp_meanimg.py
+++ scripts/step00_qc/qc02_temp_meanimg.py
@@ -27,21 +27,47 @@ task = 'pain'
 task_list = ['vicarious', 'cognitive']
 total_list = list(itertools.product(task_list, sub_list, ses_list, run_list))
 
-fmriprep_nomask = pd.DataFrame(index = list(sub_list), columns = column_list)
-fmriprep_fmriprepmask = pd.DataFrame(index = list(sub_list), columns = column_list)
-fmriprep_canlab = pd.DataFrame(index = list(sub_list), columns = column_list)
-beta_nomask     = pd.DataFrame(index = list(sub_list), columns = column_list)
-beta_canlab     = pd.DataFrame(index = list(sub_list), columns = column_list)
-
 for i, (task, sub, ses, run) in enumerate(total_list):
+    # create or load existing pandas ________________________________________________________________
+    # TODO: create a function for this
+    csv_fmriprep_fmriprepmask = os.path.join(htmlsave_dir,f'task-{task}_nifti-fmriprep_mask-fmriprep.csv')
+    if not csv_fmriprep_fmriprepmask:
+        fmriprep_fmriprepmask = pd.DataFrame(index = list(sub_list), columns = column_list)
+    else:
+        fmriprep_fmriprepmask = pd.read_csv(csv_fmriprep_fmriprepmask)
+
+    csv_fmriprep_nomask = os.path.join(htmlsave_dir,f'task-{task}_nifti-fmriprep_mask-fmriprep.csv')
+    if not csv_fmriprep_nomask:
+        fmriprep_nomask = pd.DataFrame(index = list(sub_list), columns = column_list)
+    else:
+        fmriprep_nomask = pd.read_csv(csv_fmriprep_nomask)
+
+    csv_beta_nomask = os.path.join(htmlsave_dir,f'task-{task}_nifti-beta_mask-nomask.csv')
+    if not csv_beta_nomask:
+        beta_nomask = pd.DataFrame(index = list(sub_list), columns = column_list)
+    else:
+        beta_nomask = pd.read_csv(csv_beta_nomask)
+
+    csv_beta_canlab = os.path.join(htmlsave_dir,f'task-{task}_nifti-beta_mask-canlab.csv')
+    if not csv_beta_canlab:
+        beta_canlab = pd.DataFrame(index = list(sub_list), columns = column_list)
+    else:
+        beta_canlab = pd.read_csv(csv_beta_canlab)
+
+    # fmriprep_nomask = pd.DataFrame(index = list(sub_list), columns = column_list)
+    # fmriprep_fmriprepmask = pd.DataFrame(index = list(sub_list), columns = column_list)
+    # fmriprep_canlab = pd.DataFrame(index = list(sub_list), columns = column_list)
+    # beta_nomask     = pd.DataFrame(index = list(sub_list), columns = column_list)
+    # beta_canlab     = pd.DataFrame(index = list(sub_list), columns = column_list)
     image_filename = f"{sub}_{ses}_task-social_acq-mb8_run-{run}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
     image_fullname = os.path.join(fmriprep_dir, sub, ses, 'func', image_filename)
     mask_filename = f"{sub}_{ses}_task-social_acq-mb8_run-{run}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz"
     fmriprep_mask = os.path.join(fmriprep_dir, sub, ses, 'func',  mask_filename)
     image_path = Path(image_fullname)
+    mean_filename = os.path.join(htmlsave_dir, f'{sub}_{ses}_run-{run:02d}-{task}_ev-stim_mean.nii.gz')
     if image_path.is_file():
         beta_list = glob.glob(os.path.join(beta_dir,sub, f'{sub}_{ses}_run-{run:02d}-{task}_ev-stim*.nii' )) 
-        if beta_list:
+        if not mean_filename and beta_list:
             A = nl.image.mean_img(beta_list) 
             A.to_filename(os.path.join(htmlsave_dir, f'{sub}_{ses}_run-{run:02d}-{task}_ev-stim_mean.nii.gz'))
             print(A.get_fdata().shape) # (73, 86, 73)
@@ -58,7 +84,13 @@ for i, (task, sub, ses, run) in enumerate(total_list):
             # plot gray matter mask and data alignment
             nl.plotting.plot_roi(masker.mask_img_, A)
 
-fmriprep_fmriprepmask.to_csv(os.path.join(htmlsave_dir,f'task-{task}_nifti-fmriprep_mask-fmriprep.csv'))
-beta_nomask.to_csv(os.path.join(htmlsave_dir,f'task-{task}_nifti-beta_mask-nomask.csv'))
-beta_canlab.to_csv(os.path.join(htmlsave_dir,f'task-{task}_nifti-beta_mask-canlab.csv'))
+fmriprep_fmriprepmask.to_csv(csv_fmriprep_fmriprepmask)
+beta_nomask.to_csv(csv_beta_nomask)
+beta_canlab.to_csv(beta_canlab)
 # %%
+# FIX:
+# /dartfs-hpc/rc/home/1/f0042x1/.conda/envs/spacetop_env/lib/python3.7/site-packages/nilearn/plotting/displays.py:667: 
+# RuntimeWarning: More than 20 figures have been opened. 
+# Figures created through the pyplot interface (`matplotlib.pyplot.figure`) are retained until explicitly closed and may consume too much memory. 
+# (To control this warning, see the rcParam `figure.max_open_warning`).
+#   facecolor=facecolor)
