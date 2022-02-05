942a24622a893b6c1af881e530d12f165a0f4ff4
jungheejung <>
2022-02-04 23:58:34 -0500

    check run length

---
+++ scripts/step00_qc/qc05_check_run_length/r01_tasksocial.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""Visualizes expected run length
+
+Each task has expected run length. Cross compare. 
+visualize on plotly
+
+TODO:
+1. go in to behaviorla dir
+2. for each subject session, run, extract metadata
+3. grab column param_experiment_duration
+4. save in pandas
+
+"""
+# %% libraries
+from genericpath import exists
+import dash_table
+import dash
+from pathlib import Path
+from plotly.subplots import make_subplots
+import os, glob
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import plotly.io as pio
+from datetime import datetime
+import numpy as np
+import itertools
+
+__author__ = "Heejung Jung"
+__copyright__ = "Spatial Topology Project"
+__credits__ = ["Heejung"] # people who reported bug fixes, made suggestions, etc. but did not actually write the code.
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Heejung Jung"
+__email__ = "heejung.jung@colorado.edu"
+__status__ = "Development" 
+
+# %% directories _____________________________________________________________
+current_dir = os.getcwd()
+main_dir = Path(current_dir).parents[2]
+data_dir = os.path.join(main_dir, 'data', 'dartmouth', 'd02_preprocessed' )
+child_data = next(os.walk(data_dir))[1]
+remove_int = [1,2,3,4,5]
+remove_list = [f"sub-{x:04d}" for x in remove_int]
+sub_list = [x for x in child_data if x != remove_list]
+
+# %% parameters _____________________________________________________________
+runlength = {
+    'task-social':398.76
+
+}
+# %%
+ses_list = [1,3,4]
+sub_ses = list(itertools.product(sorted(sub_list), ses_list))
+task = 'task-social'
+column_list = ['sub','ses','run','run_type','meta','experiment_dur']
+total_df = pd.DataFrame(columns = column_list)
+# %% SANDBOX
+sub_list = ['sub-0014']
+ses_list = [1,3,4]
+sub_ses = list(itertools.product(sorted(sub_list), ses_list))
+for i, (sub, ses_ind) in enumerate(sub_ses):
+    try: 
+        print( sub, ses_ind)
+        ses = f"ses-{ses_ind:02d}"
+        run_list = glob.glob(os.path.join(data_dir, sub, ses, f"{sub}_{ses}_task-social_*_beh.csv"))
+        # if run_list:
+        for ind, run_fpath in enumerate(run_list):
+            new_df = pd.DataFrame(columns = column_list)
+            run_fname = os.path.basename(run_fpath)
+            # run = run_fname.split('_')
+            run_type = run_fname.split('_')[3]
+            run_df = pd.read_csv(run_fpath)
+            new_df['sub'] = sub; new_df['ses'] = ses; new_df['run'] = run_type;
+            new_df['meta'] = f"{sub}_{ses}_{run_type}"
+            new_df['experiment_dur'] = run_df['param_experiment_duration']
+            df_concat = total_df.append(new_df])
+        
+    # TODO: PICKUP
+    # append dataframe
+    except:
+        print(f"error in {sub}_{ses}")
+        # save error in log file
+    # if file exists
+# %%
+open pd 
+df = pd.read_csv(filename)
+df['param_experiment_duration']
+
+'/sub-0030/ses-01/sub-0030_ses-01_task-social_run-01-cognitive_beh.csv'
