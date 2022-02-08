1e275aab0a5f759a5fad854c8a1f5be528b4e014
jungheejung <>
2022-02-08 12:37:03 -0500

    plot run length

--- scripts/step00_qc/qc05_check_run_length/r01_tasksocial.py
+++ scripts/step00_qc/qc05_check_run_length/r01_tasksocial.py
@@ -55,36 +55,49 @@ sub_ses = list(itertools.product(sorted(sub_list), ses_list))
 task = 'task-social'
 column_list = ['sub','ses','run','run_type','meta','experiment_dur']
 total_df = pd.DataFrame(columns = column_list)
+flag = []
 # %% SANDBOX
-sub_list = ['sub-0014']
+# sub_list = ['sub-0014']
 ses_list = [1,3,4]
 sub_ses = list(itertools.product(sorted(sub_list), ses_list))
 for i, (sub, ses_ind) in enumerate(sub_ses):
     try: 
-        print( sub, ses_ind)
+        # print( sub, ses_ind)
         ses = f"ses-{ses_ind:02d}"
         run_list = glob.glob(os.path.join(data_dir, sub, ses, f"{sub}_{ses}_task-social_*_beh.csv"))
         # if run_list:
         for ind, run_fpath in enumerate(run_list):
-            new_df = pd.DataFrame(columns = column_list)
+            # new_df = pd.DataFrame(np.zeros(1,len(column_list)), columns = column_list)
             run_fname = os.path.basename(run_fpath)
-            # run = run_fname.split('_')
+            run = run_fname.split('_')
             run_type = run_fname.split('_')[3]
             run_df = pd.read_csv(run_fpath)
-            new_df['sub'] = sub; new_df['ses'] = ses; new_df['run'] = run_type;
-            new_df['meta'] = f"{sub}_{ses}_{run_type}"
-            new_df['experiment_dur'] = run_df['param_experiment_duration']
-            df_concat = total_df.append(new_df])
-        
-    # TODO: PICKUP
+            # new_df.loc[new_df.index[0], 'sub'] = sub
+            # new_df.loc[new_df.index[0], 'ses'] = ses; 
+            # new_df.loc[new_df.index[0], 'run'] = run_type;
+            # new_df.loc[new_df.index[0], 'meta'] = f"{sub}_{ses}_{run_type}"
+            # new_df.loc[new_df.index[0], 'experiment_dur'] = run_df['param_experiment_duration'][0]
+            # print(f"{sub}_{ses}_{run_type}")
+            # total_df.append(new_df,axis=1)
+            total_df = total_df.append({'sub':sub,'ses':ses,
+            'run':int(run_type.split('-')[1]),
+            'run_type':run_type,
+            'task':run_type.split('-')[2],
+            'meta':f"{sub}_{ses}_{run_type}", 
+            'experiment_dur':run_df['param_experiment_duration'][0]}, 
+            ignore_index=True)
+            # print(total_df)
+        # total_df = pd.concat(total_df)
+# df_concat = pd.concat(total_df)
     # append dataframe
     except:
         print(f"error in {sub}_{ses}")
+        flag.append(f"{sub}_{ses}_{run_type}")
         # save error in log file
     # if file exists
 # %%
-open pd 
-df = pd.read_csv(filename)
-df['param_experiment_duration']
+filename = os.path.join(current_dir, 'experiment_length.csv')
+total_df.to_csv(filename, index=False)
+# df['param_experiment_duration']
 
-'/sub-0030/ses-01/sub-0030_ses-01_task-social_run-01-cognitive_beh.csv'
+# '/sub-0030/ses-01/sub-0030_ses-01_task-social_run-01-cognitive_beh.csv'
