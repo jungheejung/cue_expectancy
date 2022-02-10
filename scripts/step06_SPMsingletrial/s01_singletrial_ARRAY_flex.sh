d2c000269591253cc8990948248aa76fdaefcfbb
jungheejung <>
2022-02-10 15:41:36 -0500

    identify intersection of biopac and fmriprep

--- scripts/step06_SPMsingletrial/s01_singletrial_ARRAY_flex.sh
+++ scripts/step06_SPMsingletrial/s01_singletrial_ARRAY_flex.sh
@@ -10,17 +10,19 @@
 #SBATCH --partition=standard
 #SBATCH --array=31
 
-subjects=(6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 23 24 25 26 28 29 30 31 32 33 35 37 43 47 51 53 55 58 60)
+# find the conjunction of biopac TTL directories and 
+# fmriprepped derectories. 
+# subjects=(6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 23 24 25 26 28 29 30 31 32 33 35 37 43 47 51 53 55 58 60)
 KEYWORD='"early"'
-PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID))]}
+# PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID))]}
 echo "array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
 CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
 SCRIPT_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM"
-SUBJECT=${SLURM_ARRAY_TASK_ID//[!0-9]/}
-echo ${PARTICIPANT_LABEL} ${KEYWORD}
+# SUBJECT=${SLURM_ARRAY_TASK_ID//[!0-9]/}
+echo ${SLURM_ARRAY_TASK_ID} ${KEYWORD}
 module load matlab/r2020a
 
 #matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM')); s00_smooth($PARTICIPANT_LABEL);"
 
-matlab -nodisplay -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial')); s01_singletrial_onesubject_flex(${PARTICIPANT_LABEL}, ${KEYWORD});"
+matlab -nodisplay -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial')); s01_singletrial_onesubject_flex(${SLURM_ARRAY_TASK_ID}, ${KEYWORD});"
 
