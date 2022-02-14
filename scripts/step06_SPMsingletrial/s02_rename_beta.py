3ab0294adc3cb1e2b579b548f9272758d3d8a6c1
jungheejung <>
2022-02-14 12:22:27 -0500

    FIX BUG: count correct index - previous version relied on 1st column of pandas dataframe. There's possitbility that this 1st column may not be correct in case index was not dropped prorperly.

--- scripts/step06_SPMsingletrial/s02_rename_beta.py
+++ scripts/step06_SPMsingletrial/s02_rename_beta.py
@@ -14,7 +14,13 @@ meta_dir = os.path.join(main_dir, 'data', 'dartmouth', 'd06_singletrial_SPM')
 spm_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's01_singletrial')
 nifti_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's02_isolatenifti')
 sub_list = next(os.walk(spm_dir))[1]
-items_to_remove = ['sub-0001','sub-0002','sub-0003','sub-0004','sub-0005','sub-0006','sub-0007','sub-0008','sub-0009','sub-0010','sub-0014','sub-0015','sub-0016','sub-0018','sub-0019','sub-0020','sub-0023','sub-0024','sub-0025','sub-0021','sub-0032', 'sub-0050','sub-0030', 'sub-0035',  'sub-0043',  'sub-0080','sub-0031','sub-0033','sub-0037','sub-0026','sub-0028', 'sub-0029' ]
+items_to_remove = ['sub-0001','sub-0002','sub-0003','sub-0004','sub-0005',
+'sub-0006','sub-0007','sub-0008','sub-0009','sub-0010',
+'sub-0014','sub-0015','sub-0016','sub-0018','sub-0019','sub-0020',
+'sub-0023','sub-0024','sub-0025','sub-0021',
+'sub-0032', 
+'sub-0050','sub-0030', 'sub-0035',  'sub-0043', 
+ 'sub-0080','sub-0031','sub-0033','sub-0037','sub-0026','sub-0028', 'sub-0029' ]
 for item in items_to_remove:
     if item in sub_list:
         sub_list.remove(item)
@@ -24,7 +30,9 @@ print(sub_list)
 # sub = sub_list[0]
 for sub in sub_list:
     T = pd.read_csv(os.path.join(meta_dir, sub, f'{sub}_singletrial.csv'))
-    T['spm_index'] = T.iloc[:,0] + 1
+    T['spm_index'] = T.index.tolist() 
+    T['spm_index'] = T['spm_index'] +1
+
 
     subset = T[T.regressor == True].copy()
     subset['source_name'] = subset["spm_index"].astype(int).apply(lambda x: f'beta_{x:04d}.nii')
