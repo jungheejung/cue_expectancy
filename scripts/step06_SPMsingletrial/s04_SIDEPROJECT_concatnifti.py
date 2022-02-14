e956c4f8a520166ba6beca41e7fd88bbd2a78958
jungheejung <>
2022-02-14 17:09:03 -0500

    update scheme

--- scripts/step06_SPMsingletrial/s04_SIDEPROJECT_concatnifti.py
+++ scripts/step06_SPMsingletrial/s04_SIDEPROJECT_concatnifti.py
@@ -3,25 +3,51 @@
 # directories
 # %% libraries ________________________________________________________________________
 import os, sys, glob, shutil
+from os.path import join
 import pdb
 from pathlib import Path
 import itertools
+import nilearn as nl
+import nilearn.image as image
+import nibabel
 
 # parameter ________________________________________________________________________
 current_dir = os.getcwd()
 main_dir = Path(current_dir).parents[1] # discovery: /dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social
 
-sub_num = int(float(sys.argv[1]))
-sub = f"sub-{sub_num:04d}"
-ses = int(float(sys.argv[2]))
-run = int(float(sys.argv[3]))
-
-print("STARTING fslmerge")
-print(f"subject: {sub}")
-task_list = ['pain', 'vicarious', 'cognitive']
+# sub_num = int(float(sys.argv[1]))
+# sub = f"sub-{sub_num:04d}"
+# ses = int(float(sys.argv[2]))
+# run = int(float(sys.argv[3]))
+nifti_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's02_isolatenifti')
+# sub_nifti_dir = os.path.join(main_dir, 'analysis/fmri/spm/multivariate', 's02_isolatenifti')
+outputnifti_dir = os.path.join(main_dir, 'analysis/fmri/spm/multivariate', 's03_concatnifti')
+sub_folders = next(os.walk(nifti_dir))[1]
+sub_list = [i for i in sub_folders if i.startswith('sub-')]
+task_list = ['pain', 'vicarious', 'cognitive', 'pain-early', 'pain-late', 'pain-post', 'pain-plateau']
 event_list = ['cue', 'stim']
+total_list = list(itertools.product(sub_list, task_list, event_list))
+for i, (sub, task, event) in enumerate(total_list):
 
-sub_nifti_dir = os.path.join(main_dir, 'analysis/fmri/spm/multivariate', 's02_isolatenifti', sub)
-concat_dir = 
-#TODO: glob all the nififiles while excluding those within "exclude"
-niis = [log for log in glob.glob(os.path.join(sub_nifti_dir, '*.nii.gz')) if not os.path.isdir(log)]
+    #TODO: glob all the nififiles while excluding those within "exclude"
+    # niis = [log for log in glob.glob(os.path.join(sub_nifti_dir, '*.nii.gz')) if not os.path.isdir(log)]
+    print("nilearn file concatenation")
+    print(f"subject: {sub}")
+    niis = glob.glob(join(nifti_dir, sub, f"{sub}_*_run-*-{task}*_ev-{event}*.nii"))
+    if niis:
+        nifti_list = sorted(niis)
+        concat_nii = nl.image.concat_imgs(nifti_list)
+        concat_fname = join(outputnifti_dir, sub, f"{sub}_task-{task}_ev-{event}.nii")
+        concat_txt_fname = join(outputnifti_dir, sub, f"niftifname_{sub}_task-{task}_ev-{event}.txt")
+        concat_nii.to_filename(concat_fname)
+        print(concat_fname)
+        # if concat_txt_fname:
+        if os.path.exists(concat_txt_fname):
+            os.remove(concat_txt_fname)
+
+        with open(concat_txt_fname, "w") as f:
+            for item in nifti_list:
+                f.write("%s\n" % item)
+                # output.write(str(nifti_list))
+    else:
+        print("file empty")
