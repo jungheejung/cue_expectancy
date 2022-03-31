51812b47c48ef974449aa6eb1a9ce0d41479d9a2
jungheejung <heejung.jung@colorado.edu>
2022-03-31 12:18:46 -0400

    update smooth script

--- scripts/step04_SPM/s00_smooth_ARRAY.sh
+++ scripts/step04_SPM/s00_smooth_ARRAY.sh
@@ -8,10 +8,11 @@
 #SBATCH -e ./smooth/smooth_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=2-39
+#SBATCH --array=10-90%10
 ## -38
 
-subjects=(0 2 3 4 5 6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 23 24 25 26 28 29 30 31 32 33 35 37 43 47 51 53 55 58 60)
+# subjects=(0 2 3 4 5 6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 23 24 25 26 28 29 30 31 32 33 35 37 43 47 51 53 55 58 60)
+subjects=(1 2 3 4 5 6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 23 24 25 26 28 29 30 31 32 33 34 35 36 37 38 39 40 41 43 44 46 47 50 51 52 53 55 56 57 58 59 60 61 62 63 64 65 66 68 69 70 71 73 74 75 76 77 78 79 80 81 82 84 85 86 87 88 89 92)
 PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID))]}
 echo "array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
 CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
