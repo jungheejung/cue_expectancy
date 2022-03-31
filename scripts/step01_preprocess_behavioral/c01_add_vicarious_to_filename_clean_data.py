27211b95c60c487bba5afe215808123ba3f7449d
jungheejung <>
2022-03-31 12:33:37 -0400

    clean and extract rating from behavioral data

--- scripts/step01_preprocess_behavioral/c01_add_vicarious_to_filename_clean_data.py
+++ scripts/step01_preprocess_behavioral/c01_add_vicarious_to_filename_clean_data.py
@@ -4,10 +4,21 @@ import pathlib, glob
 import ntpath
 import numpy as np
 # if filename has keyword - vicarious
-list_sub = [18,23,25]
-main_dir = '/Users/h/Documents/projects_local/d_beh'
+
+# TODO: 
+# 1. get subject list
+# 2. remove unwanted participants e.g. 0999
+# 3. run script
+list_sub = list(range(107))
+main_dir = '/Users/h/Dropbox/projects_dropbox/d_beh'
 fix_file_list = []
-for ind,sub in enumerate(list_sub):
+items_to_remove = [1, 26 ]
+for item in items_to_remove:
+    if item in list_sub:
+        list_sub.remove(item)
+sub_list = sorted(list_sub)
+
+for ind,sub in enumerate(sub_list):
 
     raw_dir = os.path.join(main_dir, 'sub-{:04d}'.format(sub),
     'task-social')
@@ -17,13 +28,10 @@ for ind,sub in enumerate(list_sub):
 flat_list = [item for sub_list in fix_file_list for item in sub_list]
 
 # load and insert "vicarious" to param_task_name
-for filepath in flat_list:
-    data = pd.DataFrame();
-    #print("filepath: {0}".format(filepath))
-    #filename = ntpath.basename(filepath)
-    #num = re.findall('\d+', filename)
-    #print("numbers: {0}".format(num))
-    data = pd.read_csv(filepath)
-    data['param_task_name'] = 'vicarious'
-    data.drop(data.filter(regex="Unname"),axis=1, inplace=True)
-    data.to_csv(filepath, index=False)
+if flat_list:
+    for filepath in flat_list:
+        data = pd.DataFrame();
+        data = pd.read_csv(filepath)
+        data['param_task_name'] = 'vicarious'
+        data.drop(data.filter(regex="Unname"),axis=1, inplace=True)
+        data.to_csv(filepath, index=False)
