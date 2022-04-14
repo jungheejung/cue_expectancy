e17ed3aa59e11ac0aaa87d1e0696438ccbf913bb
jungheejung <>
2022-04-14 14:08:40 -0400

    update documnetation and comments

--- scripts/step06_SPMsingletrial/s04_SIDEPROJECT_concatnifti.py
+++ scripts/step06_SPMsingletrial/s04_SIDEPROJECT_concatnifti.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+concatenate single trial nifti files and keep a running list of the included files
+"""
 # TODO: use nibabel to stack niftis
 # subject, environment variable
 # directories
@@ -11,6 +15,15 @@ import nilearn as nl
 import nilearn.image as image
 import nibabel
 
+__author__ = "Heejung Jung"
+__copyright__ = "Spatial Topology Project"
+__credits__ = ["Heejung"] # people who reported bug fixes, made suggestions, etc. but did not actually write the code.
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Heejung Jung"
+__email__ = "heejung.jung@colorado.edu"
+__status__ = "Development" 
+
 # parameter ________________________________________________________________________
 current_dir = os.getcwd()
 main_dir = Path(current_dir).parents[1] # discovery: /dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social
@@ -21,10 +34,10 @@ main_dir = Path(current_dir).parents[1] # discovery: /dartfs-hpc/rc/lab/C/CANlab
 # run = int(float(sys.argv[3]))
 nifti_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's02_isolatenifti')
 # sub_nifti_dir = os.path.join(main_dir, 'analysis/fmri/spm/multivariate', 's02_isolatenifti')
-outputnifti_dir = os.path.join(main_dir, 'analysis/fmri/spm/multivariate', 's03_concatnifti')
+outputnifti_dir = os.path.join(main_dir, 'analysis', 'fmri', 'spm', 'multivariate', 's03_concatnifti')
 sub_folders = next(os.walk(nifti_dir))[1]
 sub_list = [i for i in sub_folders if i.startswith('sub-')]
-task_list = ['pain', 'vicarious', 'cognitive', 'pain-early', 'pain-late', 'pain-post', 'pain-plateau']
+task_list = ['pain*', 'vicarious', 'cognitive'] #, 'pain-early', 'pain-late', 'pain-post', 'pain-plateau']
 event_list = ['cue', 'stim']
 total_list = list(itertools.product(sub_list, task_list, event_list))
 for i, (sub, task, event) in enumerate(total_list):
@@ -36,7 +49,7 @@ for i, (sub, task, event) in enumerate(total_list):
     niis = glob.glob(join(nifti_dir, sub, f"{sub}_*_run-*-{task}*_ev-{event}*.nii"))
     if niis:
         nifti_list = sorted(niis)
-        concat_nii = nl.image.concat_imgs(nifti_list)
+        concat_nii = nl.image.concat_imgs(nifti_list, memory_level=0, auto_resample=False, verbose=0)
         concat_fname = join(outputnifti_dir, sub, f"{sub}_task-{task}_ev-{event}.nii")
         concat_txt_fname = join(outputnifti_dir, sub, f"niftifname_{sub}_task-{task}_ev-{event}.txt")
         concat_nii.to_filename(concat_fname)
