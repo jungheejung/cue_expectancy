3321f10e6cd5cf403e0abab50759c02f7a34ee51
jungheejung <heejung.jung@colorado.edu>
2022-04-16 12:43:35 -0400

    update slurm parameters based on example runs

--- scripts/step06_SPMsingletrial/s01_singletrial_ARRAY.sh
+++ scripts/step06_SPMsingletrial/s01_singletrial_ARRAY.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #SBATCH --job-name=lsa
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=10:00:00
+#SBATCH --time=05:00:00
 #SBATCH -o ./log/single_%A_%a.o
 #SBATCH -e ./log/single_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=3-64%8
+#SBATCH --array=9-64%8
 
 subjects=(0 6 7 8 9 10 11 13 14 15 16 17 18 19 20 21 23 24 25 26 28 29 30 31 32 33 35 37 43 47 51 53 55 58 60 61 62 63 64 65 66 68 69 70 71 73 74 75 76 77 78 79 80 81 82 84 85 86 87 88 89 92)
 PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID))]}
