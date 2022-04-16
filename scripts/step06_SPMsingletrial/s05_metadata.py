004c8c5d422047b42a24a0536b02c75eae19d778
jungheejung <heejung.jung@colorado.edu>
2022-04-16 14:15:51 -0400

    update metadata extraction column names

--- scripts/step06_SPMsingletrial/s05_metadata.py
+++ scripts/step06_SPMsingletrial/s05_metadata.py
@@ -39,13 +39,13 @@ param_list = [sub_list,
 ['cue', 'stim']]
 
 full_list = list(itertools.product(*param_list))
-for sub, task, ev in full_list:
-
-    subject_csv = os.path.join(main_dir, 'data', 'dartmouth', 'd06_singletrial_SPM', sub, f"{sub}_singletrial_plateau.csv" )
-    nifti_fname = os.path.join(nifti_dir, sub, f"niftifname_{sub}_task-{task}_ev-{ev}.txt")
+for sub, run_type, ev in full_list:
+    # /dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/data/d03_onset/onset03_SPMsingletrial/sub-0055
+    subject_csv = os.path.join(main_dir, 'data', 'd03_onset', 'onset03_SPMsingletrial', sub, f"{sub}_singletrial_plateau.csv" )
+    nifti_fname = os.path.join(nifti_dir, sub, f"niftifname_{sub}_task-social_run_{run_type}_ev-{ev}.txt")
 
     if os.path.exists(subject_csv) & os.path.exists(nifti_fname):
-        print(f"loading {sub} {task} {ev}")
+        print(f"loading {sub} {run_type} {ev}")
         nifti = pd.read_csv(nifti_fname, sep = '\t', header = None)
         meta = pd.read_csv(subject_csv)
     # subset task and ev, # based on text file, # only grab rows that exist
@@ -56,7 +56,7 @@ for sub, task, ev in full_list:
             f'sub-(\d+)_ses-(\d+)_run-(\d+)-([A-Za-z-]+)_task-social_ev-([A-Za-z-]+)-(\d+)', expand=True)[2].astype(int)
         nifti['run'] = nifti['fname'].str.split(
             f'sub-(\d+)_ses-(\d+)_run-(\d+)-([A-Za-z-]+)_task-social_ev-([A-Za-z-]+)-(\d+)', expand=True)[3].astype(int)
-        nifti['task'] = nifti['fname'].str.split(
+        nifti['run_type'] = nifti['fname'].str.split(
             f'sub-(\d+)_ses-(\d+)_run-(\d+)-([A-Za-z-]+)_task-social_ev-([A-Za-z-]+)-(\d+)', expand=True)[4]
         # nifti['fname'].str.split(
             # f'sub-(\d+)_ses-(\d+)_run-(\d+)-([A-Za-z-]+)_task-social_ev-([A-Za-z-]+)-(\d+)', expand=True)[4]
@@ -72,17 +72,17 @@ for sub, task, ev in full_list:
             np.in1d(x, nifti[['sub', 'ses', 'run', 'ev', 'num']])), axis=1)
         filtered = meta.loc[mask, ]
         filtered['nifti_name'] = filtered['sub'].apply(lambda x: f"sub-{x:04d}") +'_'+ filtered['sub'].apply(lambda x: f"ses-{x:02d}") +'_'+ \
-        filtered['run'].apply(lambda x: f"run-{x:02d}") +'-'+ task + '_task-social-' + filtered['num'].apply(lambda x: f"ev-stim-{x:04d}")
+        filtered['run'].apply(lambda x: f"run-{x:02d}") +'-'+ run_type + '_task-social-' + filtered['num'].apply(lambda x: f"ev-stim-{x:04d}")
     # subset = meta[(meta.task == task ) & (meta.ev == ev)].reset_index(drop = True)
 
     # %%
     # filtered = subset[subset.nifti_name  == nifti.fname]
     # filtered.rename(columns={0:"beta_index_fsl"})
-    save_fname = os.path.join(nifti_dir, sub, f"metadata_{sub}_task-{task}_ev-{ev}.csv")
+    save_fname = os.path.join(nifti_dir, sub, f"metadata_{sub}_task-social_run-{run_type}_ev-{ev}.csv")
     if os.path.exists(save_fname):
         os.remove(save_fname)
     else:
-        print(f"{sub}_task-{task}_ev-{ev} doesnt exist")
+        print(f"{sub}_task-social_run-{run_type}_ev-{ev} doesnt exist")
     filtered.to_csv(save_fname)
 
 # general
@@ -92,10 +92,10 @@ param_list = [sub_list,
 full_list = list(itertools.product(*param_list))
 for sub, ev in full_list:
 # load niftifname txt 
-    nifti_fname = os.path.join(nifti_dir, sub, f"niftifname_{sub}_task-general_ev-{ev}.txt")
+    nifti_fname = os.path.join(nifti_dir, sub, f"niftifname_{sub}_task-social_run-general_ev-{ev}.txt")
     nifti = pd.read_csv(nifti_fname, sep = '\t', header = None)
     # load f'{sub}_singletrial.csv
-    subject_csv = os.path.join(main_dir, 'data', 'dartmouth', 'd06_singletrial_SPM', sub, f"{sub}_singletrial.csv" )
+    subject_csv = os.path.join(main_dir, 'data', 'd03_onset', 'onset03_SPMsingletrial', sub, f"{sub}_singletrial.csv" )
     meta = pd.read_csv(subject_csv)
     # subset task and ev, # based on text file, # only grab rows that exist
     nifti['fname'] = nifti[0].map(lambda x: x.lstrip('./').rstrip('.nii'))
@@ -104,9 +104,9 @@ for sub, ev in full_list:
     # %%
     filtered = subset[subset.nifti_name  == nifti.fname]
     filtered.rename(columns={0:"beta_index_fsl"})
-    save_genfname = os.path.join(nifti_dir, sub, f"metadata_{sub}_task-general_ev-{ev}.csv")
+    save_genfname = os.path.join(nifti_dir, sub, f"metadata_{sub}_task-social_run-general_ev-{ev}.csv")
     if os.path.exists(save_genfname):
         os.remove(save_genfname)
     else:
-        print(f"{sub}_task-general_ev-{ev} doesnt exist")
+        print(f"{sub}_task-social_run-general_ev-{ev} doesnt exist")
     filtered.to_csv(save_genfname)
