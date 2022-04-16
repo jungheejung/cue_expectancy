69b8e99d4bac9536c8d0a8cd34a43532deef4bcb
jungheejung <heejung.jung@colorado.edu>
2022-04-16 13:51:51 -0400

    FIX: broken filename

--- scripts/step06_SPMsingletrial/s04_SIDEPROJECT_concatnifti.py
+++ scripts/step06_SPMsingletrial/s04_SIDEPROJECT_concatnifti.py
@@ -59,22 +59,24 @@ for i, (sub, run_type, event) in enumerate(total_list):
     print(f"subject: {sub}")
     niis = glob.glob(join(nifti_dir, sub, f"{sub}_*_run-*-{run_type}_task-social_ev-{event}*.nii"))
     if niis:
+        concat_dir = join(outputnifti_dir, sub)
+        Path(join(concat_dir)).mkdir(parents=True, exist_ok=True)
         if run_type=='vicarious' or run_type=='cognitive':
             nifti_list = sorted(niis)
             concat_nii = nl.image.concat_imgs(nifti_list, memory_level=0, auto_resample=False, verbose=0)
-            concat_fname = join(outputnifti_dir, sub, f"{sub}_task-social_run-{run_type}_ev-{event}.nii")
+            concat_fname = join(concat_dir, f"{sub}_task-social_run-{run_type}_ev-{event}.nii")
             concat_nii.to_filename(concat_fname)
-            concat_txt_fname = join(outputnifti_dir, sub, f"niftifname_{sub}_task-social_run-{run_type}_ev-{event}.txt")
+            concat_txt_fname = join(concat_dir, f"niftifname_{sub}_task-social_run-{run_type}_ev-{event}.txt")
             _rewrite_concatfname(concat_txt_fname, nifti_list)
-            print(concat_nii)
+            print(f"runtype: {run_type}, {concat_fname}")
 
-        if run_type=='pain':
+        if run_type=='pain*':
             nifti_list = sorted(niis)
             concat_nii = nl.image.concat_imgs(nifti_list, memory_level=0, auto_resample=False, verbose=0)
-            concat_fname = join(outputnifti_dir, sub, f"{sub}_task-social_run-pain_ev-{event}.nii")
+            concat_fname = join(concat_dir, f"{sub}_task-social_run-pain_ev-{event}.nii")
             concat_nii.to_filename(concat_fname)
-            concat_txt_fname = join(outputnifti_dir, sub, f"niftifname_{sub}_task-social_run-{run_type}_ev-{event}.txt")
+            concat_txt_fname = join(concat_dir, f"niftifname_{sub}_task-social_run-pain_ev-{event}.txt")
             _rewrite_concatfname(concat_txt_fname, nifti_list)
-            print(concat_nii)
+            print(f"runtype: {run_type}, {concat_fname}")
     else:
         print("file empty")
