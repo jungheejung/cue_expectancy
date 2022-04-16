1f68743417460651b233b98ddf695e4e9a41a5ba
jungheejung <heejung.jung@colorado.edu>
2022-04-16 12:20:32 -0400

    update filename scheme

--- scripts/step06_SPMsingletrial/s02_rename_beta.py
+++ scripts/step06_SPMsingletrial/s02_rename_beta.py
@@ -71,9 +71,9 @@ for sub in sub_list:
     for ind, row in subset.iterrows():
 #        print(ind, row)
         source_name = os.path.join(singletrial_dir, sub, row.source_name)
-        #nifti_name = f"sub-{row['sub']:04d}_ses-{row['ses']:02d}_run-{row['run']:02d}-{row['run_type']}_task-social_ev-{row['ev']}-{int(row['spm_index']):04d}.nii"
-        nifti_name = f"{row['nifti_name']}.nii"
-	dest_name = os.path.join(output_dir, sub, nifti_name)
+        nifti_name = f"sub-{row['sub']:04d}_ses-{row['ses']:02d}_run-{row['run']:02d}-{row['run_type']}_task-social_ev-{row['ev']}-{int(row['num']):04d}.nii"
+        #nifti_name = f"{row['nifti_name']}.nii"
+        dest_name = os.path.join(output_dir, sub, nifti_name)
         Path(os.path.join(output_dir, sub)).mkdir(parents=True, exist_ok=True)
         print(source_name)
         print(dest_name)
