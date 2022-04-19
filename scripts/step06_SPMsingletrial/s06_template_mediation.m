bcf54af85efe8a60436d0b48d37454925646efa9
jungheejung <heejung.jung@colorado.edu>
2022-04-19 10:41:12 -0400

    update template

--- scripts/step06_SPMsingletrial/s06_template_mediation.m
+++ scripts/step06_SPMsingletrial/s06_template_mediation.m
@@ -1,10 +1,13 @@
-function s06_template_mediation(task, event, csv, y_rating)
+function s06_template_mediation(run_type, event, csv, y_rating)
 % TODO:
 % 1. grab nifti and unzip
 % 2. grab metadata
 % 3. provide input as XMY
 % 4. create folder
-
+run_type = string(run_type)
+event = string(event);
+csv = string(csv);
+y_rating = string(y_rating);
 addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MediationToolbox'));
 addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore'));
 addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'));
@@ -15,17 +18,20 @@ rmpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12/external/fieldtrip/external/st
 main_dir = fileparts(fileparts(fileparts(pwd)));
 nifti_dir = fullfile(main_dir, 'analysis','fmri','spm','multivariate','s03_concatnifti')
 
-sublist = [3,4,5,6,7,8,9,10,14,15,16,18,19,20,21,23,24,25,26,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60];
+% sublist = [3,4,5,6,7,8,9,10,14,15,16,18,19,20,21,23,24,25,26,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60];
+sublist = [6,7,8,9,10,11,13,14,15,16,17,20,21,23,24,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60,61,62,64,65,66,68,69,70,71,73,74,75,76,77,78,79,80,81,84,85];
 X = cell(1, length(sublist));
 M = cell(1, length(sublist));
 Y = cell(1, length(sublist));
-
-
+% TODO: glob. if file exists, then run
+% if event == 'cue'
+%  else if event == 'stim'
+%x_col = 
 for s = 1:length(sublist)
     % step 01 __________________________________________________________________
     % grab metadata
     sub = strcat('sub-', sprintf("%04d", sublist(s)));
-    fname = strcat('metadata_', sub ,'_task-', task, '_ev-', event, '.csv');
+    fname = strcat('metadata_', sub ,'_task-social_run-', run_type, '_ev-', event, '.csv');
     T = readtable(fullfile(nifti_dir, sub, fname));
     basename = strrep(strrep(fname,'metadata_',''), '.csv', '');
 
@@ -38,11 +44,14 @@ for s = 1:length(sublist)
 
     % step 03 __________________________________________________________________
     % provide input as XMY
-    X{1, s} = T(:,event);% T.cue; %
+    X{1, s} = table2array(T(:, 'cue_con'));% T.cue; %
     M{1, s} = char(fname_nii);
-    Y{1, s} = T(:,strcat(y_rating, '_rating')); %T.actual_rating;
+    Y{1, s} = table2array(T(:,strcat(y_rating, '_rating'))); %T.actual_rating;
 
 end
+disp(X);
+disp(M);
+disp(Y);
 SETUP.mask = which('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore/canlab_canonical_brains/Canonical_brains_surfaces/gray_matter_mask.nii');
 SETUP.preprocX = 0;
 SETUP.preprocY = 0;
@@ -73,7 +82,7 @@ SETUP.fdr_p_thresh, 'size', 5, ...
 
 % Results for Path a*b
 % Generate results for the mediation effect. Here, we'll return some clusters structures with results to the workspace as output so we can examine them later. (We can also load them from disk).
-printhdr(strcat('Path a*b: Brain Mediators (M) of ',event '(X) Effects on ', task))
+printhdr(strcat('Path a*b: Brain Mediators (M) of ',event, '(X) Effects on ', run_type))
 
 [clpos, clneg, clpos_data, clneg_data, clpos_data2, clneg_data2] = mediation_brain_results('ab', 'thresh', ...
     SETUP.fdr_p_thresh, 'size', 5, ...
@@ -81,5 +90,4 @@ printhdr(strcat('Path a*b: Brain Mediators (M) of ',event '(X) Effects on ', tas
 
 %    load('cl_M-Y_pvals_003_k5_noprune.mat')
 %    whos cl*
-end
 end
