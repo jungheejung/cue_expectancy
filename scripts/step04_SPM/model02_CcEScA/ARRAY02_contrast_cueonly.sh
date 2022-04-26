f7e0e69bcb470b96fda1a6697f2b6413d61576c3
jungheejung <>
2022-04-25 21:49:10 -0400

    contrast - update filepath

--- scripts/step04_SPM/model02_CcEScA/ARRAY02_contrast_cueonly.sh
+++ scripts/step04_SPM/model02_CcEScA/ARRAY02_contrast_cueonly.sh
@@ -1,28 +1,33 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=5
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH -o ./log_con/contrast_%A_%a.o
 #SBATCH -e ./log_con/contrast_%A_%a.e
 #SBATCH --account=DBIC
-#SBATCH --partition=preemptable
-#SBATCH --array=1-25%5
-## --array=1-17%5
+#SBATCH --partition=standard
+#SBATCH --array=1-50%5
 
-subjects=( "'0002'" "'0003'" "'0004'" "'0005'" "'0006'" "'0007'" "'0008'" "'0009'" "'0010'" "'0011'" \
-"'0013'" "'0014'" "'0015'" "'0016'" "'0017'" "'0018'" "'0019'"  "'0020'" "'0021'" "'0023'" \
-"'0024'" "'0025'" "'0026'" "'0028'" "'0029'")
-
-#subjects=(2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,20)
-PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID -1))]}
+subjects=(7 8 9 10 \
+11 13 14 15 16 17 18 19 20 \
+21 23 24 28 29 30 \
+31 32 33 35 36 37 38 39 40 \
+41 43 44 46 47 50 \
+51 52 53 55 56 57 58 59 60 \
+61 62 64 65 66 68 69 70 \
+71 73 74 75 76 77 78 79 80 \
+81)
+PARTICIPANT_LABEL=${subjects[$((SLURM_ARRAY_TASK_ID - 1 ))]}
 echo "array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
-CANLABCORE_DIR="'/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore'"
-SCRIPT_DIR="'/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop/social/scripts/step04_SPM/model02_CcEScA'"
-SUBJECT=${SLURM_ARRAY_TASK_ID//[!0-9]/}
-echo ${SUBJECT}
-# sub_list = {2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,23,24,25}
-module load matlab/r2020a
-matlab -nodisplay -nosplash -batch "addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath(${CANLABCORE_DIR})); addpath(genpath(${SCRIPT_DIR})); s02_contrast_cueonly($PARTICIPANT_LABEL);"
+CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
+SCRIPT_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step04_SPM"
+FMRIPREP_DIR="'/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/derivatives/fmriprep'"
+SMOOTH_DIR="'/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop/derivatives/smooth_6mm'"
+MAIN_DIR="'/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social'"
 
+
+module load matlab/r2021a 
+#-nosplash -nojvm -nodesktop -r
+matlab -nodisplay -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step04_SPM/model02_CcEScA')); s02_contrast_cueonly(${PARTICIPANT_LABEL});"
