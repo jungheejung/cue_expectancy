aedaed020151f13aa1a8dc9d940091db3bbc1fb2
jungheejung <>
2022-05-05 19:21:09 -0400

    merge

--- scripts/step06_SPMsingletrial/smooth-6mm_task-social_run-vicarious_med-cue-cue-expect/s06_mediation_ARRAY.sh
+++ scripts/step06_SPMsingletrial/smooth-6mm_task-social_run-vicarious_med-cue-cue-expect/s06_mediation_ARRAY.sh
@@ -36,27 +36,4 @@ CANLABCORE_DIR="'/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore'"
 SPM12_DIR="'/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'"
 
 module load matlab/r2020a
-#echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
-#IND=$((SLURM_ARRAY_TASK_ID+1))
-#INFILE=`awk -F '\r\t\n' "NR==${IND}" ./s06_mediation_list.txt`
-#INFILE=`awk -F ','  "NR==${IND}" ./s06_mediation_list.txt`
-#echo $INFILE
-#task=$(echo $INFILE | cut -d, -f1)
-#x_event=$(echo $INFILE | cut -d, -f2)
-#csv=$(echo $INFILE | cut -d, -f3)
-#y_rating=$(echo $INFILE | cut -d, -f4)
-#echo ${task} ${x_event} ${csv} ${y_rating}
-
-# create folder and move template script to said folder
-#FOLDER=task-smooth-${task}_med-cue-${csv}
-#FOLDER="smooth-6mm_task-social_run-${task}_med-cue-${csv}"
-#FOLDER_FULLPATH="'/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial/${FOLDER}'"
-#mkdir -p ${FOLDER}
-#cp s06_template_mediation.m ${FOLDER}
-#cd ${FOLDER}
-# mediation
-task=\'${task}\'
-x_event=\'${x_event}\'
-csv=\'${csv}\'
-y_rating=\'${y_rating}\'
 matlab -nodisplay -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  addpath(${SPM12_DIR}); addpath(genpath(${CANLABCORE_DIR})); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial'));s06_template_mediation"
