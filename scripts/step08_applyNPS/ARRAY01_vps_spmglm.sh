fc7927d4967946908f0b5992bd13c2c117e4ec29
jungheejung <>
2022-05-10 18:19:24 -0400

    vps

---
+++ scripts/step08_applyNPS/ARRAY01_vps_spmglm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --job-name=nps
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=06:00:00
+#SBATCH -o ./log/nps_%A_%a.o
+#SBATCH -e ./log/nps_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=1-80
+module load matlab/r2020a
+
+matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step08_applyNPS')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MasksPrivate')); applyNPS_spmglm(${SLURM_ARRAY_TASK_ID});"
+
