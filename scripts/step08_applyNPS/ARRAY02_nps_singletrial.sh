59a8d74cb50e3abb0ffd717b18a3dfb81f68a78d
jungheejung <heejung.jung@colorado.edu>
2022-05-12 11:58:33 -0400

    submit single trial script

--- scripts/step08_applyNPS/ARRAY02_nps_singletrial.sh
+++ scripts/step08_applyNPS/ARRAY02_nps_singletrial.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
-#SBATCH --job-name=nps
+#SBATCH --job-name=single
 #SBATCH --nodes=1
 #SBATCH --ntasks=3
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=06:00:00
-#SBATCH -o ./log/nps_%A_%a.o
-#SBATCH -e ./log/nps_%A_%a.e
+#SBATCH --time=02:00:00
+#SBATCH -o ./log/singlenps_%A_%a.o
+#SBATCH -e ./log/singlenps_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=20-29%5
-module load matlab/r2020a
+#SBATCH --array=2
+module load matlab/r2021a
 
-matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step08_applyNPS')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MasksPrivate')); applyNPS_spmglm(${SLURM_ARRAY_TASK_ID});"
+matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step08_applyNPS')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MasksPrivate')); applyNPS_singletrial(${SLURM_ARRAY_TASK_ID});"
 
