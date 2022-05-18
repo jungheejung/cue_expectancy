626f07a9a553b9ba49522e740f7b230fd9351143
jungheejung <>
2022-05-17 21:58:06 -0400

    pdm mediation

---
+++ scripts/step05_mediationPDM/multivariate_mediation.m
@@ -0,0 +1,89 @@
+clear; close all;
+
+%%
+% xx : N subject x 1 cell
+% yy : N subject x 1 cell
+% mm : 
+run_type = 'cognitive'; % string(run_type)
+event = 'stim'; %string(event);
+csv = 'cue-actual'; %string(csv);
+y_rating = 'actual';% string(y_rating);
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MediationToolbox'));
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore'));
+addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'));
+rmpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12/external/fieldtrip'));
+rmpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12/external/fieldtrip/external/stats');
+
+addpath(genpath('/Users/h/Documents/MATLAB/MediationToolbox'));
+addpath(genpath('/Users/h/Documents/MATLAB/CanlabCore'));
+addpath(genpath('/Users/h/Documents/MATLAB/spm12'));
+rmpath(genpath('/Users/h/Documents/MATLAB/spm12/external/fieldtrip'));
+rmpath('/Users/h/Documents/MATLAB/spm12/external/fieldtrip/external/stats');
+% parameters __________________________________________________________________
+% nifti_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/analysis/fmri/fsl/multivariate/concat_nifti';
+main_dir = fileparts(fileparts(fileparts(pwd)));
+main_dir = '/Volumes/spacetop_projects_social';
+nifti_dir = fullfile(main_dir, 'analysis','fmri','spm','multivariate','s03_concatnifti');
+
+% sublist = [3,4,5,6,7,8,9,10,14,15,16,18,19,20,21,23,24,25,26,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60];
+sublist = [6,7,8,9,10,11,13,14,15,16,17,21,23,24,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60,61,62,64,65,66,68,69,70,73,74,76,78,79,80,81,84,85];
+sublist = [6,7,8,9,10,11,13,14,15,16,17,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60,61,62,64,65,66,68,69,70,73,76,78,79,80,81,84,85]; 
+% sublist = [6,7,8];
+xx = cell( length(sublist), 1); 
+mm = cell( length(sublist), 1);
+mm_fdata = cell( length(sublist), 1);
+yy = cell( length(sublist), 1);
+% TODO: glob. if file exists, then run
+% if event == 'cue'
+%  else if event == 'stim'
+%x_col = 
+for s = 1:length(sublist)
+    % step 01 __________________________________________________________________
+    % grab metadata
+    sub = strcat('sub-', sprintf("%04d", sublist(s)));
+    fname = strcat('metadata_', sub ,'_task-social_run-', run_type, '_ev-', event, '.csv');
+    T = readtable(fullfile(nifti_dir, sub, fname));
+    basename = strrep(strrep(fname,'metadata_',''), '.csv', '');
+
+    % step 02 __________________________________________________________________
+    % grab nifti and unzip
+    fname_nifti = fullfile(nifti_dir, sub, strcat(basename, '.nii.gz'));
+    fname_nii = fullfile(nifti_dir, sub, strcat(basename, '.nii'));
+    if ~exist(fname_nii,'file'), gunzip(fname_nifti)
+    end
+
+    % step 03 __________________________________________________________________
+    % provide input as XMY
+    xx{s, 1} = T.stim_lin; % table2array(T(:, 'cue_con'));% T.cue; %
+    dat =  fmri_data(char(fname_nii));
+    mm{s, 1} = dat.dat;
+%     mm{s, 1} = char(fname_nii);
+    yy{s, 1} = T.actual_rating;% table2array(T(:,strcat(y_rating, '_rating'))); %T.actual_rating;
+
+end
+
+%% Reduce the dimensionality of the brain-mediator data using PVD
+
+% project onto lower dimensional space keeping th max number of components
+pdm = multivariateMediation(xx,yy,mm,'noPDMestimation');
+
+% same as above, but keep only 25 components
+pdm = multivariateMediation(xx,yy,mm,'noPDMestimation','B',25);
+
+% If you have one trial/condition per subject use SVD option
+pdm = multivariateMediation(xx,yy,mm,'B',20,'svd');
+
+%% Compute the multivariate brain mediators
+
+% use previous PVD dimension reduction, compute all 25 PDMs, and plot path coeff
+pdm = multivariateMediation(pdm,'nPDM',25,'plots');
+
+% select the number of PDMs (3) based on the |ab| coeff plot, like a scree-plot
+pdm = multivariateMediation(pdm,'nPDM',3);
+
+
+%% bootstrap voxel weights for significance
+
+% bootstrap the first PDM with 100 samples
+pdm = multivariateMediation(pdm,'noPDMestimation','bootPDM',1,'Bsamp',100);
+pdm = multivariateMediation(xx,yy,mm,'B',25,'nPDM',3,'bootPDM',1:3,'bootJPDM','Bsamp',100,'save2file','PDMresults.mat');
