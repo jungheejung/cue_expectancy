d625346506cd08787df240f5a300a991283a80bb
jungheejung <heejung.jung@colorado.edu>
2022-05-26 02:14:53 -0400

    FIX: missing save dir

--- scripts/step05_mediationPDM/pdm_03general_l2.m
+++ scripts/step05_mediationPDM/pdm_03general_l2.m
@@ -27,7 +27,7 @@ disp(main_dir); %main_dir = '/Volumes/spacetop_projects_social';
 nifti_dir = fullfile(main_dir,'analysis','fmri','spm','multivariate','s03_concatnifti');
 save_dir = fullfile(main_dir,'analysis','fmri','mediation','pdm');
 sublist = [6,7,8,9,10,11,13,14,15,16,17,21,23,24,28,29,30,31,32,33,35,37,43,47,51,53,55,58,60,61,62,64,65,66,68,69,70,73,74,76,78,79,80,81,84,85];
-
+task_subfldr = fullfile(save_dir, strcat('task-',run{r},'_', x_keyword, '-', m_keyword,'-',y_keyword, '_l2norm'));
 % input variables
 cue_input = struct();
 
