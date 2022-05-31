642f77ba5d820604c03c5cec028af1a1e17f0ff2
jungheejung <heejung.jung@colorado.edu>
2022-05-30 22:17:21 -0400

    update l2norm

--- scripts/step06_SPMsingletrial/s06_l2norm_ARRAY.sh
+++ scripts/step06_SPMsingletrial/s06_l2norm_ARRAY.sh
@@ -4,22 +4,22 @@
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=5gb
 #SBATCH --time=01:00:00
-#SBATCH -o ./log/exclude_%A_%a.o
-#SBATCH -e ./log/exclude_%A_%a.e
+#SBATCH -o ./log_l2norm/exclude_%A_%a.o
+#SBATCH -e ./log_l2norm/exclude_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=2,4,5,6
+#SBATCH --array=3-10
 
 MAINDIR=/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 IND=$((SLURM_ARRAY_TASK_ID+1))
-INFILE=`awk -F '\r\t\n' "NR==${SLURM_ARRAY_TASK_ID}" ./s06_tasklist.txt`
+INFILE=`awk -F '\r\t\n' "NR==${SLURM_ARRAY_TASK_ID}" ./s06_l2normtasklist.txt`
 echo $INFILE
-TASK=$(echo $INFILE | cut -d$'\t' -f1)
-EVENT=$(echo $INFILE | cut -d$'\t' -f2)
+TASK=$(echo $INFILE | cut -d$' ' -f1)
+EVENT=$(echo $INFILE | cut -d$' ' -f2)
 
 module load matlab/r2020a
-matlab -nodisplay -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial')); s06_l2normniftis(${TASK}, ${EVENT});"
+matlab -nodisplay -nosplash -batch "opengl('save','hardware'); rootgroup = settings; rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = 'v7.3'; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = 'v7.3';  addpath('/dartfs-hpc/rc/lab/C/CANlab/modules/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step06_SPMsingletrial')); s06_l2normniftis('"${TASK}"', '"${EVENT}"');"
 
 
 
