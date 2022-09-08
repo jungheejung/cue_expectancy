aaef49758cce3697fb5a42cf078682fdc2547fc8
jungheejung <heejung.jung@colorado.edu>
2022-09-08 12:13:21 -0400

    MAINT: change job parameters

--- scripts/step08_applyNPS/ARRAY02_nps_singletrial.sh
+++ scripts/step08_applyNPS/ARRAY02_nps_singletrial.sh
@@ -8,7 +8,8 @@
 #SBATCH -e ./log/singlenps_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=5-8
+#SBATCH --array=1-80%10
+####5-8
 module load matlab/r2021a
 
 matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_social/scripts/step08_applyNPS')); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MasksPrivate')); applyNPS_singletrial(${SLURM_ARRAY_TASK_ID});"
