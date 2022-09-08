3edcbe7f13a053f56e0e709eabef9b40ca20e61b
jungheejung <heejung.jung@colorado.edu>
2022-09-08 18:47:29 -0400

    MAINT: move qc related files to qc folder

---
+++ scripts/qc01_identify_flatbeta.py
@@ -0,0 +1,9 @@
+import os, glob
+import pandas as pd
+
+# TODO:
+# identify which runs have flat betas
+# read tsv via pandas - grab group mriqc spreadsheet
+# spit out subset of metrics for flat beta single trials
+
+# Q. 
