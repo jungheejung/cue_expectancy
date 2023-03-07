6b4f50838a1fbd8c7c7f74ad50a6550bca6026ce
jungheejung <heejung.jung@colorado.edu>
2023-03-06 19:36:25 -0500

    BUG: indicate correct filepath

--- scripts/step10_nilearn/singletrialLSS/SUBMIT02_applysignature.sh
+++ scripts/step10_nilearn/singletrialLSS/SUBMIT02_applysignature.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH -o ./log_glm/GLM_%A_%a.o
 #SBATCH -e ./log_glm/GLM_%A_%a.e
 #SBATCH --account=DBIC
@@ -14,4 +14,4 @@ conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-${MAINDIR}/scripts/step10_nilearn/step02_applysignature_recursive.py --slurm_id ${ID} 
+${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step02_applysignature_recursive.py  --slurm_id ${ID} 
