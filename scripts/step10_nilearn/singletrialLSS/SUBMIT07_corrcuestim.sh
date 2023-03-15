6852cb2c9510e4a0579c555def2359597eb45309
jungheejung <heejung.jung@colorado.edu>
2023-03-15 01:39:35 -0400

    TST: test out paramters

--- scripts/step10_nilearn/singletrialLSS/SUBMIT07_corrcuestim.sh
+++ scripts/step10_nilearn/singletrialLSS/SUBMIT07_corrcuestim.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --time=06:00:00
-#SBATCH -o ./log_glm/GLM_%A_%a.o
-#SBATCH -e ./log_glm/GLM_%A_%a.e
+#SBATCH -o ./log_corr/GLM_%A_%a.o
+#SBATCH -e ./log_corr/GLM_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-3
+#SBATCH --array=1
 
 conda activate spacetop_env
-# echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
-# ID=$((SLURM_ARRAY_TASK_ID-1))
 RUNTYPELIST=("pain" "vicarious" "cognitive")
 RUNTYPE=${RUNTYPELIST[${SLURM_ARRAY_TASK_ID}]}
 echo ${RUNTYPE}
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-python ${MAINDIR}/scripts/step10_nilearn/step07_corr_cue_stim.py --runtype ${RUNTYPE}
+python ${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step07_corr_cue_stim.py --runtype "pain"
