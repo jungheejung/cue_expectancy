24d6e0b83326ae932f0abd1d11cb37a95494c76a
jungheejung <heejung.jung@colorado.edu>
2023-03-30 16:47:10 -0400

    ENH: update paramter

--- scripts/step10_nilearn/singletrialLSS/SUBMIT07_corrcuestim.sh
+++ scripts/step10_nilearn/singletrialLSS/SUBMIT07_corrcuestim.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --mem-per-cpu=8gb
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=16gb
 #SBATCH --time=06:00:00
 #SBATCH -o ./log_corr/GLM_%A_%a.o
 #SBATCH -e ./log_corr/GLM_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1
+#SBATCH --array=1-5
+##5
+##5
+##5
+##5
+##99
 
 conda activate spacetop_env
 # RUNTYPELIST=("pain" "vicarious" "cognitive")
@@ -16,4 +21,4 @@ conda activate spacetop_env
 # echo ${RUNTYPE}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-python ${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step07_corr_cue_stim.py --sub ${ID} --runtype "pain"
+python ${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step07_corr_cue_stim.py --slurm_id ${ID} #--runtype "pain"
