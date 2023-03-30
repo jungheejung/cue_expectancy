f60548bc709e2310ec19fcf34da1caa0f66234aa
jungheejung <heejung.jung@colorado.edu>
2023-03-30 16:46:33 -0400

    BUG: run matlab script

--- scripts/step10_nilearn/singletrialLSS/SUBMIT02_extractROI.sh
+++ scripts/step10_nilearn/singletrialLSS/SUBMIT02_extractROI.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=01:00:00
+#SBATCH --time=05:00:00
 #SBATCH -o ./log_roi/GLM_%A_%a.o
 #SBATCH -e ./log_roi/GLM_%A_%a.e
 #SBATCH --account=DBIC
@@ -14,10 +14,4 @@ echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
 module load matlab/r2020a
-matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore'));  addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MasksPrivate'));addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/Neuroimaging_Pattern_Masks/Atlases_and_parcellations')); ${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step02_extract_ROI();"
-
-
-
-
-
-
+matlab -nodisplay -nosplash -batch "addpath('/optnfs/el7/spm/spm12'); addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore'));  addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/MasksPrivate'));addpath(genpath('/dartfs-hpc/rc/lab/C/CANlab/modules/Neuroimaging_Pattern_Masks/Atlases_and_parcellations')); ${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step02_extract_ROI;"
