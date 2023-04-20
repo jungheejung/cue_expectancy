5537ca619b546c7004dddccccc54ad4a9613101c
jungheejung <heejung.jung@colorado.edu>
2023-04-19 21:33:07 -0400

    BUG: corresponding environment

--- scripts/step10_nilearn/RSA/RSA01_SUBMIT.sh
+++ scripts/step10_nilearn/RSA/RSA01_SUBMIT.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=8gb
-#SBATCH --time=05:00:00
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=12gb
+#SBATCH --time=01:00:00
 #SBATCH -o ./log_rsa/rsa_%A_%a.o
 #SBATCH -e ./log_rsa/rsa_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
 #SBATCH --array=1-133%10
+#33%10
 
-conda activate spacetop_env
+conda activate rsa
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
