ad8d7213c49033f2c4b19f77a2945bc4baf58731
jungheejung <>
2023-05-22 18:33:39 -0400

    ENH: additional variables that allow you to toggle off ablines

--- scripts/step02_R/utils/plot_twovariable.R
+++ scripts/step02_R/utils/plot_twovariable.R
@@ -1,6 +1,6 @@
 # summarize dataframe __________________________________________________________
 plot_twovariable <- function(df, iv1, iv2, group, subject, xmin, xmax, ymin,ymax,
-xlab, ylab, ggtitle, color_scheme, alpha, fit_lm, lm_method = NULL) {
+xlab, ylab, ggtitle, color_scheme, alpha, fit_lm, lm_method = NULL, identity_line=TRUE, size=NULL) {
     # x: iv1 e.g. expect rating
     # y: iv2 e.g. outcome rating
     # group: param_cue_type
@@ -25,7 +25,8 @@ xlab, ylab, ggtitle, color_scheme, alpha, fit_lm, lm_method = NULL) {
         aes(
             x = .data[["DV1_mean_per_sub"]],
             y = .data[["DV2_mean_per_sub"]],
-            color = .data[[group]]
+            color = .data[[group]],
+            size = size
         )
     ) +
         geom_point(
@@ -33,13 +34,10 @@ xlab, ylab, ggtitle, color_scheme, alpha, fit_lm, lm_method = NULL) {
             color = .data[[group]]),
             size = 2,
             alpha = alpha) +
-        geom_abline(
-            intercept = 0, slope = 1, color = "green",
-            linetype = "dashed", linewidth = 0.5
-        ) +
+
         theme(aspect.ratio = 1) +
         scale_color_manual(values = color_scheme) +
-        scale_shape_manual(values = c(16, 17)) +
+        scale_shape_manual(values = c(16, 3)) +
         xlab(xlab) +
         ylab(ylab) +
         ylim(ymin,ymax) +
@@ -54,9 +52,19 @@ xlab, ylab, ggtitle, color_scheme, alpha, fit_lm, lm_method = NULL) {
 
     if (isTRUE(fit_lm)) {
         g <- g +
-        geom_ribbon(stat = "smooth", method = lm_method, se = TRUE, alpha = 0.1,
-              aes(color = NULL, group = factor(group))) +
-        geom_line(stat = "smooth", method = lm_method, alpha = 0.8, linewidth = 1.5)
+        # geom_ribbon(stat = "smooth", method = lm_method, se = FALSE, alpha = 0.1,
+        #       aes(color = NULL, group = factor(group))) +
+        geom_line(stat = "smooth", method = lm_method, se = FALSE, alpha = 0.8, linewidth = 1.5)
+    } else {
+        g
+    }
+
+    if (isTRUE(identity_line)) {
+        g <- g + geom_abline(
+            intercept = 0, slope = 1, color = "#373737", # color = "green"
+            linetype = "dashed", 
+            linewidth = .5
+        )
     } else {
         g
     }
