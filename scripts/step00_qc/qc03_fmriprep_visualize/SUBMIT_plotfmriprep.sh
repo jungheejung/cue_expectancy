0f2b42f303dde1ab4a5dc971e2d6d4d0d3d31d48
jungheejung <heejung.jung@colorado.edu>
2023-06-01 12:04:56 -0400

    DOC: change slurm paramters for submission

--- scripts/step00_qc/qc03_fmriprep_visualize/SUBMIT_plotfmriprep.sh
+++ scripts/step00_qc/qc03_fmriprep_visualize/SUBMIT_plotfmriprep.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #SBATCH --job-name=plot
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --mem-per-cpu=8gb
-#SBATCH --time=01:00:00
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem-per-cpu=10gb
+#SBATCH --time=02:00:00
 #SBATCH -o ./logplot/GLM_%A_%a.o
 #SBATCH -e ./logplot/GLM_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-13%10
+#SBATCH --array=1-4%10
 
 conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
