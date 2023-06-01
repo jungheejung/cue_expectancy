71d30adc3a2dd72da3058ebc3ce5edfc5cca76b9
jungheejung <>
2023-06-01 12:04:19 -0400

    DEV: plot numpy

--- scripts/step00_qc/qc03_fmriprep_visualize/SUBMIT_plotfmriprep.sh
+++ scripts/step00_qc/qc03_fmriprep_visualize/SUBMIT_plotfmriprep.sh
@@ -14,9 +14,9 @@ conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-FMRIPREPDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep'
+NPYDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep_qc/numpy_bold'
 OUTPUTDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/figure/fmri/qc/fmriprep_bold_correlation'
 python ${MAINDIR}/scripts/step00_qc/qc03_fmriprep_visualize/qc05_plotfmriprep.py \
 --slurm-id ${ID} \
---fmriprepdir ${FMRIPREPDIR} \
+--inputdir ${NPYDIR} \
 --outputdir ${OUTPUTDIR}
