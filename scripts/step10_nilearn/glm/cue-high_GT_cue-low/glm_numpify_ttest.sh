68f15b61a52533d6adaae93b2f305823b1af8695
jungheejung <heejung.jung@colorado.edu>
2023-06-06 16:38:16 -0400

    STY: slurm parameters

--- scripts/step10_nilearn/glm/cue-high_GT_cue-low/glm_numpify_ttest.sh
+++ scripts/step10_nilearn/glm/cue-high_GT_cue-low/glm_numpify_ttest.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=12gb
 #SBATCH --time=01:00:00
-#SBATCH -o ./log_rsa/rsa_%A_%a.o
-#SBATCH -e ./log_rsa/rsa_%A_%a.e
+#SBATCH -o ./log/rsa_%A_%a.o
+#SBATCH -e ./log/rsa_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
 #SBATCH --array=1-133%10
