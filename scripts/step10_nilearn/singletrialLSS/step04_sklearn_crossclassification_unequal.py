aa8c3e3cc3f7c29986918dc15b90d79776d6e932
jungheejung <heejung.jung@colorado.edu>
2023-06-06 16:14:32 -0400

    DEV: balance unequal trials

--- scripts/step10_nilearn/singletrialLSS/step04_sklearn_crossclassification_unequal.py
+++ scripts/step10_nilearn/singletrialLSS/step04_sklearn_crossclassification_unequal.py
@@ -9,24 +9,6 @@ from sklearn.linear_model import LassoCV
 from sklearn.metrics import r2_score, accuracy_score
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
-############ schaefer 2018
-    # from nilearn import datasets
-    # from nilearn.maskers import NiftiLabelsMasker
-    # dataset = datasets.fetch_atlas_schaefer_2018()
-    # atlas_filename = dataset.maps
-    # # labels = dataset.labels
-    # labels = np.insert(dataset.labels, 0, 'Background')
-    # masker = NiftiLabelsMasker(labels_img=atlas_filename, standardize=True,
-    #                         memory='nilearn_cache', verbose=5)
-    # if atlas == True: 
-    #     arr = np.empty((0, len(dataset['labels'])), int)  
-    # elif atlas == False:
-    #     get_shape = glob.glob(os.path.join(
-    #             singletrial_dir, sub, f'{sub}_{ses}_run-01_runtype-*_event-stimulus_trial-*_cuetype-high_stimintensity-high.nii.gz'))
-    #     get_shape_data = image.mean_img(image.concat_imgs(get_shape)).get_fdata().ravel()
-    #     arr = np.empty((0, get_shape_data.shape[0]), int)
-    # 
-############
 
 ############# subcortical mask
 # subcortical 
@@ -81,17 +63,16 @@ def filter_good_data(filenames, baddata_df):
     
     return good_data
 
-
 def get_Xdata(singletrial_dir, sub_list, runtype, event, ntrials, mask_fname, badruns_fname):
-    # TODO: use a generic brain mask and discard voxels that are outside of the brain
-    # resample. nearest neighbors
-    # nimg.get_fdata()[gm_mask]
-    # niftimasker
+
+    # if sub_list is empty, grab every data in the singletrial_dir
     if not sub_list:
         sub_folders = next(os.walk(singletrial_dir))[1]
         sub_bids_list = [i for i in sorted(sub_folders) if i.startswith('sub-')]
         sub_list = [int(re.findall(r'\d+', item)[0].lstrip('0')) for item in sub_bids_list]
     print(sub_list)
+
+    # apply graymatter mask to single trial data
     graymattermask = image.load_img(mask_fname)
     target_singletrial =  image.concat_imgs(
         glob.glob(os.path.join(singletrial_dir, f'sub-{sub_list[0]:04d}',
@@ -125,19 +106,12 @@ def get_Xdata(singletrial_dir, sub_list, runtype, event, ntrials, mask_fname, ba
             print(gooddata_high)
             gooddata_high_concat = pd.concat([gooddata_high_concat, pd.DataFrame(gooddata_high)], axis = 0, ignore_index=True)
             gooddata_low_concat = pd.concat([gooddata_low_concat, pd.DataFrame(gooddata_low)],axis = 0,  ignore_index=True)#gooddata_low_concat.append(gooddata_low)
-
             print(f"subject id: {sub}, number of high cues: {len(gooddata_high.filename)}, number of low cues: {len(gooddata_low.filename)}")
             assert len(gooddata_high) == len(gooddata_low) # == ntrials  
-            # print(highcuelist)
             hlist = sorted(gooddata_high.filename);    llist = sorted(gooddata_low.filename)
             stacked_highcue = image.concat_imgs(sorted(hlist));    stacked_lowcue = image.concat_imgs(sorted(llist))
             masked_high = masking.apply_mask(stacked_highcue, mask, dtype='f', smoothing_fwhm=None, ensure_finite=True)
             masked_low = masking.apply_mask(stacked_lowcue, mask, dtype='f', smoothing_fwhm=None, ensure_finite=True)
-            # img_shape = masked_high.shape
-            # voxellength = img_shape[0] * img_shape[1] * img_shape[2]
-            # subwise_highcue = masked_high.get_fdata().reshape(voxellength, 36)
-            # subwise_lowcue = masked_low.get_fdata().reshape(voxellength, 36)
-            # subwise_cue = np.hstack((subwise_highcue, subwise_lowcue))
             subwise_cue = np.vstack((masked_high, masked_low))
             X = np.append(X, subwise_cue, axis=0)
             print(X.shape)
@@ -146,14 +120,16 @@ def get_Xdata(singletrial_dir, sub_list, runtype, event, ntrials, mask_fname, ba
     gooddata_highdf = pd.concat([gooddata_highdf, gooddata_high_concat], ignore_index=True)
     gooddata_lowdf = pd.concat([gooddata_lowdf, gooddata_low_concat], ignore_index=True)
     gooddatadf = pd.concat([gooddata_highdf, gooddata_lowdf], ignore_index=True)
-    #assert X.shape[0] == len(sub_list)*ntrials*2
+
     return X, gooddatadf #gooddata_highdf, gooddata_lowdf
+
     ####################### check alignment between single trial and graymatter mask
     # display = plotting.plot_stat_map(image.mean_img(target_singletrial),display_mode='mosaic',
     #                    cut_coords=(5, 4, 10),
     #                    title="display_mode='z', cut_coords=5")
     # display.add_overlay(mask,cmap=plotting.cm.purple_green)
     ############################################################################################
+
     ####################### convert back to brain ##############################################
     # import nibabel as nib
     # new_image = nib.Nifti1Image(
@@ -173,26 +149,6 @@ def get_Xdata(singletrial_dir, sub_list, runtype, event, ntrials, mask_fname, ba
     ############################################################################################
 
 
-# %% ver1
-"""
-Version 1 doesn't work. 
- Cannot have number of splits n_splits=100 greater than the number of groups: 2.
-
-N = 100
-ntrials = 36
-y = np.tile(np.tile(np.repeat(['high', 'low'], ntrials), N), 2)# high cue, low cue
-X = np.random.randn(len(y))
-subjects = np.tile(np.repeat(np.arange(N), ntrials*2), 2)
-groups = np.repeat(['pain', 'cognitive'], N*ntrials*2)
-assert len(y) == len(subjects) == len(groups)
-# group_kfold = GroupKFold(n_splits=N)
-group_kfold = LeavePGroupsOut(n_groups=N)
-for i, (train_index, test_index) in enumerate(group_kfold.split(X, y, groups)):
-    print(f"Fold {i}:")
-    print(f"  Train: index={train_index}, group={groups[train_index]}")
-    print(f"  Test:  index={test_index}, group={groups[test_index]}")
-"""
-
 
 # %% ver 2 # predefined split
 singletrial_dir = '/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/singletrial'
@@ -204,10 +160,8 @@ event = 'stimulus'
 ntrials = 36 # average maps
 
 sub_list = [] #list(np.arange(1,134))
+sub_list = [77, 84, 11, 31]
 badruns_fname = '/Users/h/Documents/projects_local/cue_expectancy/scripts/step00_qc/qc03_fmriprep_visualize/bad_runs.json'
-# baddf = json_extractbids(badruns_fname)
-
-
 
 
 # %%
@@ -215,11 +169,10 @@ N = len(sub_list)
 # y = np.tile(np.repeat(['high', 'low'], ntrials), N) # high cue, low cue
 
 mask_fname = '/Users/h/Documents/MATLAB/CanlabCore/CanlabCore/canlab_canonical_brains/Canonical_brains_surfaces/gray_matter_mask.nii.gz'
-X_pain, paindf = get_Xdata(singletrial_dir, sub_list = sub_list, runtype = 'pain', event='stimulus', ntrials=36, mask_fname = mask_fname, badruns_fname = badruns_fname)
+X_painstim, paindf = get_Xdata(singletrial_dir, sub_list = sub_list, runtype = 'pain', event='stimulus', ntrials=36, mask_fname = mask_fname, badruns_fname = badruns_fname)
 X_cognitive, cogdf = get_Xdata(singletrial_dir, sub_list = sub_list, runtype = 'cognitive', event='stimulus', ntrials=36, mask_fname = mask_fname, badruns_fname = badruns_fname)
 
 # extract metadata from filenames
-
 pattern = r'sub-(\d+)_ses-(\d+)_run-(\d+)_runtype-(\w+)_event-(\w+)_trial-(\d+)_cuetype-(\w+)_stimintensity-(\w+)\.nii\.gz'
 keywords = paindf['filename'].str.extract(pattern)
 keywords.columns = ['sub', 'ses', 'run', 'runtype', 'event', 'trial', 'cuetype', 'stimintensity']
@@ -231,20 +184,30 @@ keywords_cogh.columns = ['sub', 'ses', 'run', 'runtype', 'event', 'trial', 'cuet
 cogdf_meta = keywords_cogh.apply(lambda x: x.str.lstrip('0'))
 
 # %% 
-if np.sum(paindf_meta['cuetype'] == cogdf_meta['cuetype']) == len(paindf_meta['cuetype']):
-    y = np.array(paindf_meta['cuetype'])
+# if np.sum(paindf_meta['cuetype'] == cogdf_meta['cuetype']) == len(paindf_meta['cuetype']):
+    # y = np.array(paindf_meta['cuetype'])
 #### try 1
 # paindf_grouped = paindf_meta.groupby('sub').filter(lambda x: x['ses'].nunique() == 1)
 # cogdf_grouped = cogdf_meta.groupby('sub').filter(lambda x: x['ses'].nunique() == 1)
 
 # # Matching the filtered DataFrames based on the group column
 # matched_data = pd.merge(paindf_grouped, cogdf_grouped, on='Group', suffixes=('_df1', '_df2'))
+###########################################################################################################################
+### try 2 ###########################################################################################################################
+# TODO: 
+# create a function, where you compare two dataframes absed on groupby 'sub', 'ses'
+# From that, if the number of rows don't match, shuffle and drop number of rows from either dataframe that has excess number of rows
 
-### try 2
+# balance trials __________________________________________________________________________
 df1_trial_counts = paindf_meta.groupby('sub')['ses'].size()
 df2_trial_counts = cogdf_meta.groupby('sub')['ses'].size()
 diff = df1_trial_counts - df2_trial_counts
 diff_df = diff.reset_index().rename(columns={'index': 'sub', 0: 'diff'})
+# inspiration
+# https://stackoverflow.com/questions/45839316/pandas-balancing-data
+# g = df.groupby('class')
+# g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True))
+
 # sub 11, 24 diff
 selected_trial_counts = []
 pdf = paindf_meta.copy()
@@ -258,25 +221,31 @@ for index, row in filtered_diff_df.iterrows():
     rows_to_drop = []
     sub_rows = pdf[pdf['sub'] == sub]
     if len(sub_rows) >= N:
-        rows_to_drop = random.sample(sub_rows.index.tolist(), int(N))
-        pdf = pdf.drop(rows_to_drop)
+        # rows_to_drop = random.sample(sub_rows.index.tolist(), int(N))
+        # pdf = pdf.drop(rows_to_drop)
+        high_rows_to_drop = random.sample(sub_rows[sub_rows['cuetype'] == 'high'].index.tolist(), int(N/2))
+        low_rows_to_drop = random.sample(sub_rows[sub_rows['cuetype'] == 'low'].index.tolist(), int(N/2))
+        # rows_to_drop = random.sample(sub_rows.index.tolist(), int(negN))
+
+        pdf = pdf.drop(list(high_rows_to_drop) + list(low_rows_to_drop))
 
 negdiff_df = diff_df[diff_df['ses'] < 0]
 for index, row in negdiff_df.iterrows():
-    print(sub)
     sub = row['sub']
+    print(sub)
     negN = np.abs(negdiff_df.loc[negdiff_df['sub'] == sub, 'ses'].values[0])
     rows_to_drop = []
     sub_rows = cdf[cdf['sub'] == sub]
+    # TODO: equally drop two classes. 
     if len(sub_rows) >= negN:
-        rows_to_drop = random.sample(sub_rows.index.tolist(), int(negN))
-        cdf = cdf.drop(rows_to_drop)
+        high_rows_to_drop = random.sample(sub_rows[sub_rows['cuetype'] == 'high'].index.tolist(), int(negN/2))
+        low_rows_to_drop = random.sample(sub_rows[sub_rows['cuetype'] == 'low'].index.tolist(), int(negN/2))
+        # rows_to_drop = random.sample(sub_rows.index.tolist(), int(negN))
 
-# TODO: 
-create a function, where you compare two dataframes absed on groupby 'sub', 'ses'
-From that, if the number of rows don't match, shuffle and drop number of rows from either dataframe that has excess number of rows
+        cdf = cdf.drop(list(high_rows_to_drop) + list(low_rows_to_drop))
+###########################################################################################################################
 # y_cog = cogdf_meta['cuetype']
-print(f"{X_pain.shape}")
+print(f"{X_painstim.shape}")
 print(f"{X_cognitive.shape}")# X, y should be identical for both pain and cognitive
 # X = brain_maps # (conditions x subjects) x voxels . 2d (72 x subject) x voxel # NOTE: I could also average within conditions
 # %%
@@ -290,7 +259,16 @@ print(f"{X_cognitive.shape}")# X, y should be identical for both pain and cognit
 # The accuracy_score() function is used to compute the accuracy of the predictions, and the resulting score is appended to either the accuracy_cognitive or accuracy_pain list, depending on which type of data was being predicted.
 # After all iterations of the for loop, the mean accuracy scores for cognitive and pain data are printed out.
 # Overall, the code looks correct, and should perform PredefinedSplit cross-validation to generate predictions of y_cog and y_pain, and then calculate the accuracy of those predictions.
-# TODO: assert paindf['sub'] == cogdf['sub']
+
+assert pdf['cuetype'].tolist() == cdf['cuetype'].tolist()
+y = np.array(paindf_meta['cuetype'])
+
+# only grab trials that match pdf and cdf index ( given that we dropped unbalanced trials)
+X_pain = X_painstim[pdf.index]
+X_cog = X_cognitive[cdf.index]
+print(f"after balancing: {X_pain.shape}")
+print(f"{X_coX_coggnitive.shape}")
+# %%
 subjects = pd.factorize(paindf['sub'])[0]#np.repeat(np.arange(N), ntrials*2)
 # assert len(y) == len(subjects) 
 cv = PredefinedSplit(subjects)
@@ -298,17 +276,21 @@ accuracy_cognitive = []
 accuracy_pain = []
 for train_index, test_index in cv.split(X_pain, y):
     clf = svm.SVC()
-    scaler = StandardScaler()
+
+    # scaling __________________________________________
+    scaler = StandardScaler() # TODO: standard scalar. .fit_transform(X_train) -> mean/sd of the transformed data
     X_pain_train = scaler.fit_transform(X_pain[train_index]) # z-score on the samples
     X_pain_test = scaler.transform(X_pain[test_index])
     X_cog_train = scaler.fit_transform(X_cognitive[train_index]) # z-score on the samples
     X_cog_test = scaler.transform(X_cognitive[test_index])
 
-    clf.fit(X_pain_train, y[train_index]) # TODO: remove the transpose. X_pain, y. shape should match for axis 0
-    y_cog = clf.predict(X_cog_test) # TODO: standard scalar. .fit_transform(X_train) -> mean/sd of the transformed data
+    # betwee tasks prediction __________________________________________
+    clf.fit(X_pain_train, y[train_index])
+    y_cog = clf.predict(X_cog_test) 
     acc_cog = accuracy_score(y[test_index], y_cog)
     accuracy_cognitive.append(acc_cog)
 
+    # within tasks prediction __________________________________________
     y_pain = clf.predict(X_pain_test)
     acc_pain = accuracy_score(y[test_index], y_pain)
     accuracy_pain.append(acc_pain)
