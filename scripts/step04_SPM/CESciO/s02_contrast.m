814860713bb342b9c1f7590885092e693b9bc798
jungheejung <heejung.jung@colorado.edu>
2023-06-08 16:41:55 -0400

    BUG: filename typo and runlength variable

--- scripts/step04_SPM/CESciO/s02_contrast.m
+++ scripts/step04_SPM/CESciO/s02_contrast.m
@@ -101,8 +101,8 @@ c11 = []; c12 = []; c13 = []; c14 = []; c15 = []; c16 = []; c17 = []; c18 = []; 
 c21 = []; c22 = []; c23 = []; c24 = []; c25 = []; 
 
 matlabbatch = cell(1,1);
-
-for run_ind = 1: size(A,1)
+runlength = size(A,1);
+for run_ind = 1: runlength
     disp(strcat('run', num2str(run_ind)));
     sub = strcat('sub-', sprintf('%04d', A.sub_num(run_ind)));
     ses = strcat('ses-', sprintf('%02d', A.ses_num(run_ind)));
@@ -111,7 +111,7 @@ for run_ind = 1: size(A,1)
     covariate = zeros(1, size(SPM.Sess(run_ind).C.name,2));
 
     disp(strcat('[ STEP 04 ]constructing contrasts...'));
-    onset_glob    = dir(fullfile(onset_dir, sub, ses, strcat(sub, '_', ses, '_task-social_',strcat('run-', sprintf('%02d', A.run_num(run_ind))), '-*_events.tsv')));
+    onset_glob    = dir(fullfile(onset_dir, sub, ses, strcat(sub, '_', ses, '_task-cue_',strcat('run-', sprintf('%02d', A.run_num(run_ind))), '_*_events.tsv')));
     onset_fname   = fullfile(char(onset_glob.folder), char(onset_glob.name));
     if isempty(onset_glob)
       disp('ABORT')
