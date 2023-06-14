39fbe7841739c4900d7348873034ff05c6d8608e
jungheejung <heejung.jung@colorado.edu>
2023-06-14 16:29:12 -0400

    DEV: cross prediction

---
+++ scripts/step10_nilearn/cross_prediction/qc01_SUBMIT.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --job-name=plot
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=02:00:00
+#SBATCH -o ./log/skl_%A_%a.o
+#SBATCH -e ./log/skl_%A_%a.e
+#SBATCH --account=DBIC
+#SBATCH --partition=standard
+#SBATCH --array=4%10
+
+conda activate spacetop_env
+echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
+ID=$((SLURM_ARRAY_TASK_ID-1))
+MAINDIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue
+SINGLETRIALDIR=${MAINDIR}/analysis/fmri/nilearn/singletrial'
+OUTPUTDIR=${MAINDIR}/analysis/fmri/nilearn/crossprediction
+CANLABCOREDIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore"
+
+python ${PWD}/c01_sklearn_crossprediction.py \
+--slurm-id ${ID} \
+--maindir ${MAINDIR} \
+--singletrialdir ${SINGLETRIALDIR} \
+--outputdir ${OUTPUTDIR} \ 
+--canlabcoredir ${CANLABCOREDIR}
