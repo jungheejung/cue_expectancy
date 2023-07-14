BUG: computation error - just grab matrix

scripts/step10_nilearn/glm_diagnostics/corr_singletrial-SPM/ISC.py
@@ -60,9 +60,9 @@ for index in range(high_cue.shape[0]):
 fmri_masked_stimhighp = np.vstack(Hp)
 # %% 
 iscs = np.corrcoef(fmri_masked_stimhighp, rowvar=False)
-diagonal_elements = np.diagonal(iscs)
-np.save(join(save_dir, 'isc_' + os.path.basename(high_fname)), diagonal_elements)
-singletrial_t = nifti_masker.inverse_transform(diagonal_elements)
+#diagonal_elements = np.diagonal(iscs)
+np.save(join(save_dir, 'isc_' + os.path.basename(high_fname)), iscs)
+singletrial_t = nifti_masker.inverse_transform(iscs)
 resampled_image = image.resample_to_img(singletrial_t, ref_img)
 plot = plotting.plot_stat_map(resampled_image,  display_mode = 'mosaic', title = f'{os.path.splitext(os.path.basename(high_fname))[0]}', cut_coords = 8)
 plot.savefig(join(save_dir ,os.path.splitext(os.path.basename(high_fname))[0] + '.png'))