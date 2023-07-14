6b7a973ff261ee8bde89ea62c19878e650208b54
jungheejung <heejung.jung@colorado.edu>
2023-07-13 20:39:39 -0400

    DEV: ISC for average numpy

---
+++ scripts/step10_nilearn/glm_diagnostics/corr_singletrial-SPM/ISC.py
@@ -0,0 +1,69 @@
+# %%----------------------------------------------------------------------
+#                                   libraries
+# ----------------------------------------------------------------------
+import os, glob, re, gzip, shutil, json
+from os.path import join
+import pathlib
+import numpy as np
+import statsmodels 
+from statsmodels.stats import multitest
+import scipy
+import nilearn
+from scipy import stats
+from nilearn import image, plotting
+import argparse
+from nilearn.image import new_img_like, resample_to_img, math_img
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import squareform
+__author__ = "Heejung Jung"
+__copyright__ = "Spatial Topology Project"
+__credits__ = ["Heejung"] # people who reported bug fixes, made suggestions, etc. but did not actually write the code.
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Heejung Jung"
+__email__ = "heejung.jung@colorado.edu"
+__status__ = "Development" 
+
+"""
+load nifti_masker
+load numpy (it's a numpy array, averaged per subject)
+get correlation and diagonal element for isc 
+save as plot and nii
+"""
+main_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
+npy_dir = join(main_dir, 'analysis/fmri/nilearn/deriv05_singletrialnpy')
+canlab_dir = '/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore'
+save_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/fmri/nilearn/deriv07_isc'
+
+imgfname = join(main_dir, 'analysis/fmri/nilearn/singletrial/sub-0060/sub-0060_ses-01_run-05_runtype-vicarious_event-stimulus_trial-011_cuetype-low_stimintensity-low.nii.gz')
+ref_img = image.load_img(imgfname)
+
+mask = image.load_img(canlab_dir, '/CanlabCore/canlab_canonical_brains/Canonical_brains_surfaces/brainmask_canlab.nii')
+mask_img = nilearn.masking.compute_epi_mask(mask, target_affine=ref_img.affine, target_shape=ref_img.shape)
+nifti_masker = nilearn.maskers.NiftiMasker(mask_img=mask_img, smoothing_fwhm=6,
+                                            target_affine=ref_img.affine, target_shape=ref_img.shape, 
+                                            memory_level=1)
+
+x,y,z=ref_img.shape
+Hp = []
+Lp = []
+high_fname = join(npy_dir, 'sub-avg_ses-avg_run-avg_event-stimulus_cuetype-high.npy')
+high_cue = np.load(high_fname)
+
+for index in range(high_cue.shape[0]):
+
+    Hp.append(
+        nifti_masker.fit_transform(
+    new_img_like(ref_img, high_cue[index].reshape(x,y,z)))
+    )
+
+fmri_masked_stimhighp = np.vstack(Hp)
+# %% 
+iscs = np.corrcoef(fmri_masked_stimhighp, rowvar=False)
+diagonal_elements = np.diagonal(iscs)
+np.save(join(save_dir, 'isc_' + os.path.basename(high_fname)), diagonal_elements)
+singletrial_t = nifti_masker.inverse_transform(diagonal_elements)
+resampled_image = image.resample_to_img(singletrial_t, ref_img)
+plot = plotting.plot_stat_map(resampled_image,  display_mode = 'mosaic', title = f'{os.path.splitext(os.path.basename(high_fname))[0]}', cut_coords = 8)
+plot.savefig(join(save_dir ,os.path.splitext(os.path.basename(high_fname))[0] + '.png'))
+resampled_image.to_filename(join(save_dir, os.path.splitext(os.path.basename(high_fname))[0] + '.nii.gz'))
