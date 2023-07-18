7402bf45ebeee1e1b57e340f329cff80b076e20b
jungheejung <heejung.jung@colorado.edu>
2023-07-17 20:15:28 -0400

    DOC: slurm parameters

--- scripts/step10_nilearn/cross_prediction/qc01_SUBMIT.sh
+++ scripts/step10_nilearn/cross_prediction/qc01_SUBMIT.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH --job-name=plot
+#SBATCH --job-name=cc
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
 #SBATCH --mem-per-cpu=40G
-#SBATCH --time=02:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH -o ./log/skl_%A_%a.o
 #SBATCH -e ./log/skl_%A_%a.e
 #SBATCH --account=DBIC
