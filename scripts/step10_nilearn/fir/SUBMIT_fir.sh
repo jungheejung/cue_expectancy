dac5193d69dfa2bb619a0412f22f76bb53ce4d2c
jungheejung <heejung.jung@colorado.edu>
2023-08-05 17:31:15 -0400

    BUT: python script; slurm parameters

--- scripts/step10_nilearn/fir/SUBMIT_fir.sh
+++ scripts/step10_nilearn/fir/SUBMIT_fir.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=8gb
-#SBATCH --time=05:00:00
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=03:00:00
 #SBATCH -o ./log_fir/fir_%A_%a.o
 #SBATCH -e ./log_fir/fir_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-13%10
+#SBATCH --array=1-133%10
 
 conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-${MAINDIR}/scripts/step10_nilearn/fir/step01_fir_sandbox.py \
---slurm-id ${ID} \
---taskname "pain" \
---firdelay 20
+python ${MAINDIR}/scripts/step10_nilearn/fir/step01_fir_sandbox.py --slurm-id ${ID} --taskname "pain" --firdelay 20
