fc5e0df0aed7421df3bce406d430c4dc09de30a6
jungheejung <heejung.jung@colorado.edu>
2023-08-10 10:51:07 -0400

    BUG: string quotes

--- scripts/step09_FIRmatlab/FIR_SLURM.sh
+++ scripts/step09_FIRmatlab/FIR_SLURM.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #SBATCH --job-name=glm
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --mem-per-cpu=16gb
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem-per-cpu=40G
 #SBATCH --time=10:00:00
 #SBATCH -o ./log_glm/GLM_%A_%a.o
 #SBATCH -e ./log_glm/GLM_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1
+#SBATCH --array=22
 ###-133%20
 
 CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
@@ -16,7 +16,7 @@ MAIN_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue"
 ONSET_DIR="${MAIN_DIR}/data/fmri/fmri01_onset/onset02_SPM"
 FMRIPREP_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/data/spacetop_data/derivatives/fmriprep/results/fmriprep"
 BADRUNJSON="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/scripts/bad_runs.json"
-SAVE_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/fmri/spm/fir"
+SAVE_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/fmri/spm/fir/ttl1"
 SPM_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/spm12"
 ATLAS_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/Neuroimaging_Pattern_Masks/Atlases_and_parcellations"
 mylist=($(find ${FMRIPREP_DIR} -maxdepth 1 -mindepth 1 -type d -iname "sub-*"))
@@ -26,7 +26,7 @@ echo "* total of ${#mylist[@]} participants in ${INPUT_DIR}"
 echo "* array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
 
 module load matlab/r2020a
-matlab -nodesktop -nosplash -batch 'opengl("save","hardware"); rootgroup = settings;rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = "v7.3"; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = "v7.3";addpath(genpath('"'${CANLABCORE_DIR}'"'));addpath(genpath('"'${SPM_DIR}'"'));addpath(genpath('"'${ATLAS_DIR}'"'));addpath(genpath('"'${MAIN_DIR}'"'));addpath(genpath('"'${ONSET_DIR}'"'));addpath(genpath('"'${PWD}'"'));FIR_spm('"'${PARTICIPANT_LABEL}'"','"'${ONSET_DIR}'"','"'${MAIN_DIR}'"', '"'${FMRIPREP_DIR}'"', '"'${BADRUNJSON}'"', , '"'${SAVE_DIR}'"');'
+matlab -nodesktop -nosplash -batch 'opengl("save","hardware"); rootgroup = settings;rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = "v7.3"; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = "v7.3";addpath(genpath('"'${CANLABCORE_DIR}'"'));addpath(genpath('"'${SPM_DIR}'"'));addpath(genpath('"'${ATLAS_DIR}'"'));addpath(genpath('"'${MAIN_DIR}'"'));addpath(genpath('"'${ONSET_DIR}'"'));addpath(genpath('"'${PWD}'"'));FIR_spm('"'${PARTICIPANT_LABEL}'"','"'${ONSET_DIR}'"','"'${MAIN_DIR}'"', '"'${FMRIPREP_DIR}'"', '"'${BADRUNJSON}'"',  '"'${SAVE_DIR}'"');'
 
 echo "\n\nCODE:\nmatlab -nodesktop -nosplash -batch 'opengl("save","hardware"); 
 rootgroup = settings; 
@@ -37,4 +37,4 @@ addpath(genpath('"'${SPM_DIR}'"'));
 addpath(genpath('"'${MAIN_DIR}'"')); 
 addpath(genpath('"'${INPUT_DIR}'"')); 
 addpath(genpath('"'${PWD}'"'))
-s01_glm('"'${PARTICIPANT_LABEL}'"','"'${INPUT_DIR}'"','"'${MAIN_DIR}'"','"'${FMRIPREP_DIR}'"', '"'${BADRUNJSON}'"');'
+s01_glm('"'${PARTICIPANT_LABEL}'"','"'${INPUT_DIR}'"','"'${MAIN_DIR}'"','"'${FMRIPREP_DIR}'"', '"'${BADRUNJSON}'"');'"
