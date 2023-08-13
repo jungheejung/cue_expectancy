b08351d0497fbf4ba6834e7cfbbb5d0ec892a942
jungheejung <heejung.jung@colorado.edu>
2023-08-13 10:27:13 -0400

    DEV: longer timeseries

--- scripts/step09_FIRmatlab/FIR_spm.m
+++ scripts/step09_FIRmatlab/FIR_spm.m
@@ -8,9 +8,9 @@
 % %%%%%%%% 
 
 function FIR_spm(sub, onset_dir, main_dir, fmriprep_dir, badruns_json, save_dir)
-
+disp(strcat('--------------------',sub,'----------------'));
 TR = 0.46;
-T = 10;
+T = 20;
 mode = 0;
 atlas_obj = load_atlas('canlab2018_2mm');
 labels = atlas_obj.labels;
@@ -91,7 +91,7 @@ A = intersect( intersectRuns(:, inter_num_column), sortedonsetT(:, onset_num_col
 disp(A);
 
 %% 3. for loop "run-wise" _______________________________________________________
-for run_ind = 1:1%size(A, 1)
+for run_ind = 1:size(A, 1)
     disp(strcat('______________________run', num2str(run_ind), '____________________________'));
     % [x] extract sub, ses, run info
     sub = []; ses = []; run = [];
@@ -196,7 +196,7 @@ fmriprep_fname = fullfile(fmriprep_dir, sub, ses, 'func', strcat(sub, '_', ses, 
 fmridata = fmri_data(fmriprep_fname);
 [parcel_means, parcel_pattern_expression, parcel_valence, ~, ~, voxel_count]=apply_parcellation(fmridata, atlas_obj);
 % timeseries_parcel = reshape(parcel_means, [1, size(parcel_means)]);
-
+disp("------loaded fmriprep image and parcellation! --------")
 % for i=1:length(atlas_obj.labels)
 
 
@@ -219,24 +219,28 @@ for i = 1:length(fields)
     disp(strcat("runtype: ", runtype)); 
     % Create values for the additional columns to be appended
     sub_col = repmat(sub, 6, 1);    ses_col = repmat(ses, 6, 1);
-    run_col = repmat(run, 1, 1)';    runtype_col = repmat(runtype, 6, 1);
+    run_col = repmat(run, 6, 1);    runtype_col = repmat(runtype, 6, 1);
     ROI_col = repmat(key, 6, 1);    condition_col = {"cueH_stimH";  "cueL_stimH"; "cueH_stimM"; "cueL_stimM" ;"cueH_stimL" ;"cueL_stimL"};
-    
-    % Combine the data and additional columns
-    combinedData = [sub_col, ses_col, num2cell(run_col), runtype_col, ROI_col, condition_col, num2cell(data)];
-    combinedDataCell = [cellstr(sub_col), cellstr(ses_col), num2cell(run_col), ...
+    disp(sub_col);    
+    disp(size(sub_col)); disp(size(data));
+    % Combine the data and additional columnsi
+    combinedDataCell = [];
+    %combinedData = [sub_col, ses_col, run_col, runtype_col, ROI_col, condition_col, num2cell(data)];
+    combinedDataCell = [cellstr(sub_col), cellstr(ses_col), cellstr(run_col), ...
                         cellstr(runtype_col), cellstr(ROI_col), cellstr(condition_col), ...
                         num2cell(data)]; % Convert data to cell array along rows
     
     % Create a table from the combined data
     dataTable = cell2table(combinedDataCell, ...
         'VariableNames', {'sub', 'ses', 'run', 'runtype', 'ROI', 'condition', 'tr1', 'tr2', 'tr3', 'tr4', 'tr5', 'tr6', 'tr7', 'tr8', 'tr9', 'tr10', ...
-        'tr11', 'tr12', 'tr13', 'tr14', 'tr15', 'tr16', 'tr17', 'tr18', 'tr19', 'tr20'});
+        'tr11', 'tr12', 'tr13', 'tr14', 'tr15', 'tr16', 'tr17', 'tr18', 'tr19', 'tr20', 'tr21', 'tr22', 'tr23', 'tr24', 'tr25', 'tr26', 'tr27', 'tr28', 'tr29', 'tr30',...
+        'tr31', 'tr32', 'tr33', 'tr34', 'tr35', 'tr36', 'tr37', 'tr38', 'tr39', 'tr40', 'tr41', 'tr42'});
     
     % Display the resulting table
     disp(dataTable);
-    save_fname = fullfile(save_dir, sub, strcat(sub,'_',ses,'_',run,'_runtype-',runtype{1},'-roi-',key,'_tr-',size(data,2),'.tsv' ));
-    writetable(dataTable, 'sample_data.csv');
+    disp(strcat(sub, ses, run, runtype{1}, key));
+    save_fname = fullfile(save_dir, sub, strcat(sub,'_',ses,'_',run,'_runtype-',runtype{1},'-roi-',key,'_tr-42.csv' ));
+    writetable(dataTable, save_fname);
     %     H_All_Reg(:,sub,i)=h(:,1);
     %     H_All_Neg(:,sub,i)=h(:,2);
     %     H_All_Neu(:,sub,i)=h(:,3);
@@ -248,30 +252,28 @@ for i = 1:length(fields)
     plot(h(:,5), 'color', 'blue', 'LineStyle', '-');
     plot(h(:,6), 'color', 'blue', 'LineStyle', '--');
 %     save TODO:
-    save_plotname = fullfile(save_dir, sub, strcat(sub,'_',ses,'_',run,'_runtype-',runtype{1},'-roi-',key,'_tr-',size(data,2),'.png' ));
+    save_plotname = fullfile(save_dir, sub, strcat(sub,'_',ses,'_',run,'_runtype-',runtype{1},'-roi-',key,'_tr-42.png' ));
     saveas(gcf, save_plotname, 'png');
     hold off;
     %     plot(h(:,3), 'color', 'b');
 
-    [h1,t1,w1,w_times1,halfh1, auc1] = fir2htw2(h(:,1));
-    [h2,t2,w2,w_times2,halfh2, auc2] = fir2htw2(h(:,2));
-    [h3,t3,w3,w_times3,halfh3, auc3] = fir2htw2(h(:,3));
-    [h4,t4,w4,w_times4,halfh4, auc4] = fir2htw2(h(:,4));
-    [h5,t5,w5,w_times5,halfh5, auc5] = fir2htw2(h(:,5));
-    [h6,t6,w6,w_times6,halfh6, auc6] = fir2htw2(h(:,6));
-    %     [h3,t3,w3,w_times3,halfh3, auc3] = fir2htw2(h(:,3));
-    Martin_Param{i,sub}=param;
-    IndexM1(sub,i,:)=[h1,t1,w1,w_times1,halfh1, auc1];
-    IndexM2(sub,i,:)=[h2,t2,w2,w_times2,halfh2, auc2];
-    IndexM3(sub,i,:)=[h3,t3,w3,w_times3,halfh3, auc3];
-    IndexM4(sub,i,:)=[h4,t4,w4,w_times4,halfh4, auc4];
-    IndexM5(sub,i,:)=[h5,t5,w5,w_times5,halfh5, auc5];
-    IndexM6(sub,i,:)=[h6,t6,w6,w_times6,halfh6, auc6];
-    %     IndexM3(sub,i,:)=[h3,t3,w3,w_times3,halfh3, auc3];
+%    [h1,t1,w1,w_times1,halfh1, auc1] = fir2htw2(h(:,1));
+%    [h2,t2,w2,w_times2,halfh2, auc2] = fir2htw2(h(:,2));
+%    [h3,t3,w3,w_times3,halfh3, auc3] = fir2htw2(h(:,3));
+%    [h4,t4,w4,w_times4,halfh4, auc4] = fir2htw2(h(:,4));
+%    [h5,t5,w5,w_times5,halfh5, auc5] = fir2htw2(h(:,5));
+%    [h6,t6,w6,w_times6,halfh6, auc6] = fir2htw2(h(:,6));
+%    Martin_Param{i,sub}=param;
+%   IndexM1(sub,i,:)=[h1,t1,w1,w_times1,halfh1, auc1];
+%   IndexM2(sub,i,:)=[h2,t2,w2,w_times2,halfh2, auc2];
+%   IndexM3(sub,i,:)=[h3,t3,w3,w_times3,halfh3, auc3];
+%   IndexM4(sub,i,:)=[h4,t4,w4,w_times4,halfh4, auc4];
+%   IndexM5(sub,i,:)=[h5,t5,w5,w_times5,halfh5, auc5];
+%   IndexM6(sub,i,:)=[h6,t6,w6,w_times6,halfh6, auc6];
 end
 
 end
-
+end
 function [sub, ses, task, run, runtype] = extract_bids(filename)
 
 pattern = '/sub-(\w+)_ses-(\w+)_task-(\w+)_run-(\d+)_runtype-(\w+)_events\.tsv';
@@ -293,5 +295,4 @@ else
 disp('No matches found.');
 end
 
-end
 end
