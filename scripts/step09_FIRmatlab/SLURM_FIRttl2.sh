7f2f35ca1d7f449f2d7ffebadcd85ee6b5e11c20
jungheejung <heejung.jung@colorado.edu>
2023-08-13 12:15:34 -0400

    DEV: slurm parameters

--- scripts/step09_FIRmatlab/SLURM_FIRttl2.sh
+++ scripts/step09_FIRmatlab/SLURM_FIRttl2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=glm
+#SBATCH --job-name=ttl2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
 #SBATCH --mem-per-cpu=16G
 #SBATCH --time=24:00:00
-#SBATCH -o ./log_glm/GLM_%A_%a.o
-#SBATCH -e ./log_glm/GLM_%A_%a.e
+#SBATCH -o ./log_ttl2/ttl2_%A_%a.o
+#SBATCH -e ./log_ttl2/ttl2_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
 #SBATCH --array=1-133%50
