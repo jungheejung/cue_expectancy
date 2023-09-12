4826735b42ab358159770219e9165c7f1d08ae3a
jungheejung <heejung.jung@colorado.edu>
2023-09-12 14:55:28 -0400

    STY: slurm parameters

--- scripts/step09_FIRmatlab/SLURM_FIRttl2_parallel.sh
+++ scripts/step09_FIRmatlab/SLURM_FIRttl2_parallel.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -l
-#SBATCH --job-name=glm
+#SBATCH --job-name=par2
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem-per-cpu=24G
-#SBATCH --time=3-00:00:00
-#SBATCH -o ./log_glm/GLM_%A_%a.o
-#SBATCH -e ./log_glm/GLM_%A_%a.e
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=24:00:00
+#SBATCH -o ./log_par2/GLM_%A_%a.o
+#SBATCH -e ./log_par2/GLM_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-13
+#SBATCH --array=1-30%10
 
 CANLABCORE_DIR="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore/CanlabCore"
 MAIN_DIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue"
@@ -25,7 +26,7 @@ echo "* total of ${#mylist[@]} participants in ${INPUT_DIR}"
 echo "* array id: " ${SLURM_ARRAY_TASK_ID}, "subject id: " ${PARTICIPANT_LABEL}
 KEYWORD="rINS"
 
-module load matlab/r2020a
+module load matlab/r2022a
 matlab -nodesktop -nosplash -batch 'opengl("save","hardware"); rootgroup = settings;rootgroup.matlab.general.matfile.SaveFormat.PersonalValue = "v7.3"; rootgroup.matlab.general.matfile.SaveFormat.TemporaryValue = "v7.3";addpath(genpath('"'${CANLABCORE_DIR}'"'));addpath(genpath('"'${SPM_DIR}'"'));addpath(genpath('"'${ATLAS_DIR}'"'));addpath(genpath('"'${MAIN_DIR}'"'));addpath(genpath('"'${ONSET_DIR}'"'));addpath(genpath('"'${PWD}'"'));FIR_spm_ttl2_parallel('"'${PARTICIPANT_LABEL}'"','"'${ONSET_DIR}'"','"'${MAIN_DIR}'"', '"'${FMRIPREP_DIR}'"', '"'${BADRUNJSON}'"',  '"'${SAVE_DIR}'"', '"'${KEYWORD}'"');'
 
 echo "\n\nCODE:\nmatlab -nodesktop -nosplash -batch 'opengl("save","hardware"); 
