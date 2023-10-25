53902b08e9bc949a592f0e56863019577848f9bd
jungheejung <heejung.jung@colorado.edu>
2023-10-25 13:04:56 -0400

    BUG: date formate

--- scripts/step04_SPM/6conditions/s03_checkcontrast.m
+++ scripts/step04_SPM/6conditions/s03_checkcontrast.m
@@ -2,14 +2,15 @@
 % and find it in the contrast list. 
 
 % load SPM for every participant
-firstlevel_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/fmri/spm/univariate/model01_CESO/1stLevel';
+firstlevel_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/fmri/spm/univariate/model01_6cond_ttl1';
+firstlevel_dir= '/dartfs-hpc/scratch/f0042x1/spm/model01_6cond_ttl1';
 list = dir(firstlevel_dir);
 sub_dir = regexp({list.name}, 'sub-\d{4}', 'match');
 sub_list = sub_dir(~cellfun(@isempty, sub_dir));
 
-flag = []
+flag = [];
 
-for sub = 50:length(sub_list)
+for sub = 1:length(sub_list)
     load(fullfile(firstlevel_dir, char(sub_list{sub}), 'SPM.mat'));
     % SPM constructed regressor list
     descrip = find(contains({SPM.Vbeta.descrip}, 'CUE')==1);
@@ -24,8 +25,11 @@ for sub = 50:length(sub_list)
     clear SPM
 end
 
-save_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/scripts/step04_SPM/CESO';
-save_fname = fullfile(save_dir, strcat('s03_checkcontrast_', datetime('now','Format','MM-dd-yyyy'), '.txt'));
+save_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/scripts/step04_SPM/6conditions';
+currentDate = datetime('now','Format','MM-dd-yyyy').string();
+save_fname = fullfile(save_dir, "s03_checkcontrast_" + currentDate + ".txt");
+
+save_fname = fullfile(save_dir, strcat('s03_checkcontrast_', string(datetime('now', 'Format', 'MM-dd-yyyy')), '.txt'));
 fid = fopen(save_fname,'w');    % open file for writing (overwrite if necessary)
 fprintf(fid,'%s',flag);          % Write the char array, interpret newline as new line
 fclose(fid);   
