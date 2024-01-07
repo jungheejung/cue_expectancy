188ca3351bc841e55e2a7349d9c26da0af01da62
jungheejung <heejung.jung@colorado.edu>
2024-01-07 12:14:34 -0500

    STY: tidy comments

--- scripts/step02_R/utils/filter_df_ses_trial.R
+++ scripts/step02_R/utils/filter_df_ses_trial.R
@@ -30,39 +30,3 @@ filter_df_ses_trial <- function(data, session_colname, subject_colname, session_
   
   return(df_summary)
 }
-
-# filter_df_ses_trial <- function(data, session_colname, subject_colname, session_threshold, trial_threshold) {
-
-# data.2= data %>%
-#   arrange(.data[[subject_colname]] ) %>%
-#   group_by(.data[[subject_colname]]) %>%
-#   mutate(trial_number = row_number())
-
-# df_with_trial_number <- aggregate(trial_number ~ .data[[subject_colname]], data = data.2, FUN = function(x) length(unique(x)))
-# sessions_per_subject <- aggregate(.data[[session_colname]] ~ .data[[subject_colname]], data = data, FUN = function(x) length(unique(x)))
-
-# df_summary <- df_with_trial_number %>%
-#   inner_join(sessions_per_subject, by = subject_colname) %>%
-#   filter(trial_number > trial_threshold, .data[[subject_colname]] == session_threshold)
-
-#   return(df_summary)
-
-
-  
-
-# }
-
-
-# """
-# sessions_per_subject <- aggregate(session_id ~ src_subject_id, data = data, FUN = function(x) length(unique(x)))
-# data.2= data %>%
-#   arrange(src_subject_id ) %>%
-#   group_by(src_subject_id) %>%
-#   mutate(trial_number = row_number())
-
-# df_with_trial_number <- aggregate(trial_number ~ src_subject_id, data = data.2, FUN = function(x) length(unique(x)))
-# sessions_per_subject <- aggregate(session_id ~ src_subject_id, data = data_p2, FUN = function(x) length(unique(x)))
-# df_summary <- df_with_trial_number %>% 
-#   inner_join(sessions_per_subject, by = "src_subject_id") %>% 
-#   filter(trial_number > 50, session_id == 3)
-# """
--- scripts/step02_R/utils/lmer_onefactor_cooksd_fix.R
+++ scripts/step02_R/utils/lmer_onefactor_cooksd_fix.R
@@ -1,37 +1,41 @@
+#' Function to compute Cook's distance for a one-factor linear mixed-effects model
+#'
+#' This function fits a linear mixed-effects model to the specified data
+#' and calculates Cook's distance for the model, which is a measure of
+#' the influence of each observation.
+#'
+#' @param dataframe A data frame containing the data.
+#' @param taskname A string indicating the name of the task.
+#' @param iv The column name that contains the independent variable.
+#' @param dv A vector of dependent variable names.
+#' @param subject The column name indicating the subject (random effect).
+#' @param dv_keyword A string representing the dependent variable for display purposes.
+#' @param model_savefname The filename where the model summary will be saved.
+#'
+#' @return A vector of Cook's distance values for each observation in the model.
+#' @import lme4
+#' @importFrom stringr str_to_title
+#' @export
+#'
+#' @examples
+#' # Example usage:
+#' # lmer_onefactor_cooksd_fix(dataframe, "Task1", "IV1", "DV1", "Subject", "dv_keyword", "model.txt")
 lmer_onefactor_cooksd_fix <- function(dataframe, taskname, iv, dv, subject,
                                   dv_keyword, model_savefname) {
-
-      #   """
-      #   Parameters
-      #   ----------
-      #   data:
-      #         a data frame
-      #   taskname:
-      #         a string of the task. will be saved in filename
-      #   iv:
-      #         the column name that contains the variable to be summariezed
-      #   dv:
-      #         a vector containing that are between-subjects column names
-      #   random_factor:
-      #         random effects
-      #   dv_keyword:
-      #         a string of the dependent variable
-      #   model_savefname:
-      #         model save filename
-      #   """
-      library(lme4)
-      eval(substitute(iv), dataframe)
-      eval(substitute(dv), dataframe)
-      eval(substitute(subject), dataframe)
-      eval(substitute(model_savefname))
-      model_onefactor <- lmer( dv ~ iv + (iv | subject), data = dataframe)
-      print(paste("model: ", stringr::str_to_title(dv_keyword), " ratings - ", taskname))
-      print(summary(model_onefactor))
-      sink(model_savefname)
-      print(summary(model_onefactor))
-      sink()
-      fixEffect <<- as.data.frame(fixef(model_onefactor))
-      randEffect <<- as.data.frame(ranef(model_onefactor))
-      cooksd <- cooks.distance(model_onefactor)
-      return(cooksd)
+  library(lme4)
+  eval(substitute(iv), dataframe)
+  eval(substitute(dv), dataframe)
+  eval(substitute(subject), dataframe)
+  eval(substitute(model_savefname))
+  model_onefactor <- lmer( dv ~ iv + (iv | subject), data = dataframe)
+  print(paste("model: ", stringr::str_to_title(dv_keyword), " ratings - ", taskname))
+  print(summary(model_onefactor))
+  sink(model_savefname)
+  print(summary(model_onefactor))
+  # used to redirect the output from the R console to a file. 
+  sink()
+  fixEffect <<- as.data.frame(fixef(model_onefactor))
+  randEffect <<- as.data.frame(ranef(model_onefactor))
+  cooksd <- cooks.distance(model_onefactor)
+ return(cooksd)
 }
