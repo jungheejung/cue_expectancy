51d24683e2f9fb712933d9c067725457e1a749fb
jungheejung <heejung.jung@colorado.edu>
2024-01-07 18:26:50 -0500

    ENH: update hline plotting scheme with more flexibility

--- scripts/step02_R/utils/ggplot_hline_bartoshuk.R
+++ scripts/step02_R/utils/ggplot_hline_bartoshuk.R
@@ -1,17 +1,57 @@
-ggplot_hline_bartoshuk <- function(xposition) {
+#' Add horizontal lines and labels to a plot.
+#'
+#' This function adds horizontal lines and labels to a plot at predefined
+#' y-intercepts with corresponding labels.
+#'
+#' @param xposition The x-position where the lines and labels will be added.
+#'
+#' @return NULL. This function is used for side effects (adding lines and labels to a plot).
+#'
+#' @examples
+#' p <- ggplot(data, aes(x = x, y = y)) + geom_point()
+#' p + ggplot_hline_bartoshuk(3.5)
+#'
+#' @import ggplot2
+#'
+#' @export
+#' 
+# continue to developgeom_hline(yintercept = 0, size = 0.1, linetype = "dashed") +
+# geom_label(x = 3.5, y = 0, label = c("no sensation"), hjust = 0, nudge_x = 0.1, size = 3) +
+ggplot_hline_bartoshuk <- function(g, xposition, linesize = 0.1, linetype = "dashed", linecolor = "#adb5bd", nudge_x = 0.1, textsize = 9) {
     library(ggplot2)
-    geom_hline(yintercept = 3) +
-        annotate("text", x = xposition, y = 3, label = "Barely detectable", vjust = -0.5) +
-        geom_hline(yintercept = 10) +
-        annotate("text", x = xposition, y = 10, label = "Weak", vjust = -0.5) +
-        geom_hline(yintercept = 29) +
-        annotate("text", x = xposition, y = 29, label = "Moderate", vjust = -0.5) +
-        geom_hline(yintercept = 64) +
-        annotate("text", x = xposition, y = 64, label = "Strong", vjust = -0.5) +
-        geom_hline(yintercept = 96) +
-        annotate("text", x = xposition, y = 96, label = "Very Strong", vjust = -0.5) +
-        geom_hline(yintercept = 180) +
-        annotate("text", x = xposition, y = 180, label = "Strongest imaginable", vjust = -0.5)
+    # Add horizontal lines and labels to the ggplot2 object
+    g <- g +
+        # geom_hline(yintercept = 3, size = linesize, linetype = linetype) +
+        # # geom_label(x = 3.5, y = 3, label = c("barely detectable"), hjust = 0, nudge_x = 0.1, size = 3)
+        # annotate("text", x = xposition + nudge_x, y = 3, label = "Barely detectable", hjust = 0, vjust = -0.5, size = textsize) +
+        # geom_hline(yintercept = 10, size = linesize, linetype = linetype) +
+        # annotate("text", x = xposition+ nudge_x, y = 10, label = "Weak", hjust = 0, vjust = -0.5, size = textsize) +
+        # geom_hline(yintercept = 29, size = linesize, linetype = linetype) +
+        # annotate("text", x = xposition+ nudge_x, y = 29, label = "Moderate", hjust = 0, vjust = -0.5, size = textsize) +
+        # geom_hline(yintercept = 64, size = linesize, linetype = linetype) +
+        # annotate("text", x = xposition+ nudge_x, y = 64, label = "Strong", hjust = 0, vjust = -0.5, size = textsize) +
+        # geom_hline(yintercept = 96, size = linesize, linetype = linetype) +
+        # annotate("text", x = xposition+ nudge_x, y = 96, label = "Very Strong", hjust = 0, vjust = -0.5, size = textsize) +
+        # geom_hline(yintercept = 180, size = linesize, linetype = linetype) +
+        # annotate("text", x = xposition+ nudge_x, y = 180, label = "Strongest imaginable", hjust = 0, vjust = -0.5, size = textsize) 
+        # #theme(plot.margin = margin(r = unit(4, "cm")))
+
+
+        geom_segment(aes(x = 0, xend = xposition, y = 3, yend = 3), size = linesize, linetype = linetype, color = linecolor) +
+        # geom_label(x = 3.5, y = 3, label = c("barely detectable"), hjust = 0, nudge_x = 0.1, size = 3)
+        annotate("text", x = xposition + nudge_x, y = 3, label = "Barely detectable", hjust = 0, vjust = 0, size = textsize) +
+        geom_segment(aes(x = 0, xend = xposition, y = 10, yend = 10), size = linesize, linetype = linetype, color = linecolor) +
+        annotate("text", x = xposition+ nudge_x, y = 10, label = "Weak", hjust = 0, vjust = 0, size = textsize) +
+        geom_segment(aes(x = 0, xend = xposition, y = 29, yend = 29), size = linesize, linetype = linetype, color = linecolor) +
+        annotate("text", x = xposition+ nudge_x, y = 29, label = "Moderate", hjust = 0, vjust = 0, size = textsize) +
+        geom_segment(aes(x = 0, xend = xposition, y = 64, yend = 64), size = linesize, linetype = linetype, color = linecolor) +
+        annotate("text", x = xposition+ nudge_x, y = 64, label = "Strong", hjust = 0, vjust = 0, size = textsize) +
+        geom_segment(aes(x = 0, xend = xposition, y = 96, yend = 96), size = linesize, linetype = linetype, color = linecolor) +
+        annotate("text", x = xposition+ nudge_x, y = 96, label = "Very Strong", hjust = 0, vjust = 0, size = textsize) +
+        geom_segment(aes(x = 0, xend = xposition, y = 180, yend = 180), size = linesize, linetype = linetype, color = linecolor) +
+        annotate("text", x = xposition+ nudge_x, y = 180, label = "Strongest imaginable", hjust = 0, vjust = 0, size = textsize) 
+        #theme(plot.margin = margin(r = unit(4, "cm")))
+    return(g)
 }
 # abline(h = 3, lty = 2) + # "Barely detectable"
 # abline(h = 10, lty = 2) + # "Weak"
