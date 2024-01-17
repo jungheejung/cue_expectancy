756686622f324d3fb5d244a3fdbca6b3d9004524
jungheejung <heejung.jung.gr@dartmouth.edu>
2024-01-17 12:43:35 -0500

    BUG: add documentation

--- scripts/step02_R/utils/plot_halfrainclouds_onefactor.R
+++ scripts/step02_R/utils/plot_halfrainclouds_onefactor.R
@@ -1,24 +1,44 @@
+#' Plot Half Rainclouds for One Factor
+#'
+#' This function creates a half raincloud plot for a given one-factor dataset. It combines violin plots,
+#' box plots, and individual data points to provide a comprehensive view of the data distribution.
+#'
+#' @param subjectwise A data frame containing subject-wise data.
+#' @param groupwise A data frame containing group-wise summary data.
+#' @param iv Independent variable name (as a string) in the data frame.
+#' @param sub_mean The name of the subject-wise mean variable in the data frame.
+#' @param group_mean The name of the group mean variable in the data frame.
+#' @param se Standard error for the group mean.
+#' @param subject The name of the subject identifier variable in the data frame.
+#' @param ggtitle Title for the ggplot.
+#' @param title Title for the legend.
+#' @param xlab Label for the x-axis.
+#' @param ylab Label for the y-axis.
+#' @param taskname Name of the task (unused in function).
+#' @param ylim y-axis limits for the plot.
+#' @param w Width of the
+#' @param h Height of the saved plot.
+#' @param dv_keyword Keyword associated with the dependent variable (unused in function).
+#' @param color Color scheme for the plot, specified as a vector of colors.
+#' @param plot_savefname Filename for saving the plot.
+#' 
 plot_halfrainclouds_onefactor <- function(subjectwise, groupwise, iv,
-                                      subjectwise_mean, group_mean, se, subject,
+                                      sub_mean, group_mean, se, subject,
                                       ggtitle, title, xlab, ylab, taskname, ylim,
-                                      w, h, dv_keyword, color, save_fname) {
+                                      w, h, dv_keyword, color, plot_savefname) {
   g <- ggplot(
     data = subjectwise,
     aes(
-      y = .data[[subjectwise_mean]],
+      y = .data[[sub_mean]],
       x = factor(.data[[iv]]),
       fill = factor(.data[[iv]])
     )
   ) +
     coord_cartesian(ylim = ylim, expand = TRUE) +
-    # geom_flat_violin(
-    #   aes(fill = factor(.data[[iv]])),
-    #   position = position_nudge(x = .1, y = 0),
-    #   adjust = 1.5, trim = FALSE, alpha = .3, colour = NA
-    # ) +
+
 
     geom_half_violin(
-      aes(fill = factor(.data[[iv]])),
+      # aes(fill = factor(.data[[iv]])),
       side = 'r',
       position = 'dodge',
       adjust = 1.5,
@@ -27,32 +47,12 @@ plot_halfrainclouds_onefactor <- function(subjectwise, groupwise, iv,
       colour = NA
     ) +
 
-    # geom_line(
-    #   data = subjectwise,
-    #   aes(
-    #     group = .data[[subject]],
-    #     y = .data[[subjectwise_mean]],
-    #     x = as.numeric(.data[[iv]]) - .15,
-    #     fill = factor(.data[[iv]])
-    #   ),
-    #   linetype = "solid", color = "grey", alpha = .3
-    # ) +
-    # geom_point(
-    #   aes(
-    #     x = as.numeric(.data[[iv]]) - .15,
-    #     y = .data[[subjectwise_mean]],
-    #     color = factor(.data[[iv]])
-    #   ),
-    #   position = position_jitter(width = .05),
-    #   size = 1, alpha = 0.8, shape = 20
-    # ) +
-
   geom_line(data = subjectwise,
     aes(
       group = .data[[subject]],
-      x = as.numeric(as.factor(.data[[iv]])) - .15 ,
-      y = .data[[subjectwise_mean]],
-      fill = factor(.data[[iv]])
+      x = as.numeric(as.factor(.data[[iv]])) - .15,
+      y = .data[[sub_mean]],
+      # fill = factor(.data[[iv]])
       ),
     linetype = "solid",
     color = "grey",
@@ -61,28 +61,23 @@ plot_halfrainclouds_onefactor <- function(subjectwise, groupwise, iv,
   geom_point(
     aes(
       # group = .data[[subject]],
-      x = as.numeric(as.factor(.data[[iv]])) - .15 ,
-      y = .data[[subjectwise_mean]],
+      x = as.numeric(as.factor(.data[[iv]])) - .15,
+      y = .data[[sub_mean]],
       color = factor(.data[[iv]])
     ),
     position = position_jitter(width = .05),
     size = 2,
     alpha = 0.7,
   ) + 
-    # geom_boxplot(
-    #   aes(
-    #     x = .data[[iv]],
-    #     y = .data[[subjectwise_mean]],
-    #     fill = .data[[iv]]
-    #   ),
-    #   outlier.shape = NA, alpha = 0.8, width = .1, colour = "black"
-    # ) +
+
 
     geom_half_boxplot(
       data = subjectwise,
       aes(x = .data[[iv]],
-          y = .data[[subjectwise_mean]],
-          fill = .data[[iv]]),
+          y = .data[[sub_mean]]
+          #,
+          # fill = .data[[iv]]
+          ),
       side = "r",
       outlier.shape = NA,
       alpha = 0.8,
@@ -110,13 +105,13 @@ plot_halfrainclouds_onefactor <- function(subjectwise, groupwise, iv,
     expand_limits(x = 2.8) +
     #guides(fill = "none") +
     guides(color = "none") +
-    guides(fill = guide_legend(title = title)) +
+    # guides(fill = guide_legend(title = title)) +
     scale_fill_manual(values = color) +
     scale_color_manual(values = color) +
     ggtitle(ggtitle) +
     xlab(xlab) +
     ylab(ylab) +
     theme_bw()
-  ggsave(save_fname, width = w, height = h)
+  # ggsave(plot_savefname, width = w, height = h)
   return(g)
 }
