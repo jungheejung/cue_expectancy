c0ccafc5011a050a94350460179c111bdeea0e98
jungheejung <heejung.jung.gr@dartmouth.edu>
2024-01-17 22:11:29 -0500

    DOC: add roxygen

--- scripts/step02_R/utils/plot_halfrainclouds_twofactor.R
+++ scripts/step02_R/utils/plot_halfrainclouds_twofactor.R
@@ -1,3 +1,37 @@
+#' Plot Half Rainclouds for Two Factors
+#'
+#' This function creates a half raincloud plot for a given two-factor dataset. It combines
+#' violin plots, box plots, and individual data points to provide a comprehensive view of
+#' the data distribution across two independent variables.
+#'
+#' @param subjectwise A dataframe containing subject-wise data.
+#' @param groupwise A dataframe containing group-wise summary statistics.
+#' @param iv1 The name of the first independent variable in the dataframe.
+#' @param iv2 The name of the second independent variable in the dataframe.
+#' @param sub_mean The name of the subject-wise mean variable in the dataframe.
+#' @param group_mean The name of the group mean variable in the dataframe.
+#' @param se The name of the standard error variable in the dataframe.
+#' @param subject The name of the subject identifier variable in the dataframe.
+#' @param ggtitle Title for the ggplot.
+#' @param legend_title Title for the legend.
+#' @param xlab Label for the x-axis.
+#' @param ylab Label for the y-axis.
+#' @param task_name Name of the task (unused in function).
+#' @param ylim y-axis limits for the plot.
+#' @param w Width of the saved plot.
+#' @param h Height of the saved plot.
+#' @param dv_keyword Keyword associated with the dependent variable (unused in function).
+#' @param color Color scheme for the plot, specified as a named vector of colors.
+#' @param save_fname Filename for saving the plot.
+#'
+#' @return A ggplot object representing the customized half raincloud plot.
+#'
+#' @examples
+#' # Example usage (assuming appropriate data frames 'subjectwise' and 'groupwise'):
+#' plot_halfrainclouds_twofactor(subjectwise, groupwise, "IV1", "IV2",
+#'                               "SubMean", "GroupMean",
+
+
 plot_halfrainclouds_twofactor <- function(subjectwise, groupwise,
                                       iv1, iv2, sub_mean, group_mean, se, subject,
                                       ggtitle, legend_title, xlab, ylab, task_name, ylim,
@@ -11,18 +45,12 @@ plot_halfrainclouds_twofactor <- function(subjectwise, groupwise,
             fill = .data[[iv2]]
         )
     ) +
-        # geom_flat_violin(
-        #     aes(fill = .data[[iv2]]),
-        #     position = position_nudge(x = .1, y = 0),
-        #     adjust = 1.5, trim = FALSE, alpha = .3, colour = NA
-        # ) +
 
         geom_half_violin(
             data = subjectwise,
-        aes(color = .data[[iv2]]), #factor(.data[[iv2]])),
+        aes(color = .data[[iv2]]),
         side = 'r',
         position = position_nudge(x = .1, y = 0),
-        # position = 'dodge',
         adjust = 1.5,
         trim = FALSE,
         alpha = .3,
@@ -50,22 +78,6 @@ plot_halfrainclouds_twofactor <- function(subjectwise, groupwise,
             size = 1, alpha = 0.8, shape = 20
         ) +
 
-        # geom_half_boxplot(
-        # data = subjectwise,
-        # aes(x = .data[[iv1]],
-        #     y = .data[[sub_mean]],
-        #     fill = .data[[iv2]]),
-        # side = "r",
-        # outlier.shape = NA,
-        # alpha = 0.8,
-        # width = .1,
-        # notch = FALSE,
-        # notchwidth = 0,
-        # varwidth = FALSE,
-        # colour = "black",
-        # errorbar.draw = FALSE
-        # ) +
-
         geom_boxplot(
         data = subjectwise,
         aes(x = .data[[iv1]],
@@ -80,7 +92,7 @@ plot_halfrainclouds_twofactor <- function(subjectwise, groupwise,
         colour = "black",
         ) +
 
-        # use summary stats __________________________________________________________________________________ # nolint
+        # use summary stats ____________________________________________________
 
         geom_errorbar(
             data = groupwise,
@@ -95,7 +107,7 @@ plot_halfrainclouds_twofactor <- function(subjectwise, groupwise,
         ) +
 
 
-        # legend stuff __________________________________________________________________________________ # nolint
+        # legend stuff _________________________________________________________
         expand_limits(x = 3.5) +
         guides(fill = "none") +
         guides(color = "none") +
