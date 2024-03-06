cfb8dd69e1f2f8c2b1366fa306b549e090ee47e8
jungheejung <heejung.jung@colorado.edu>
2024-03-06 14:45:47 -0500

    BUG: glob file, not subfolder

--- scripts/step10_nilearn/singletrialLSS/step02_filtervif.py
+++ scripts/step10_nilearn/singletrialLSS/step02_filtervif.py
@@ -9,18 +9,18 @@ In this code, we filter the single trials based on a specific VIF threshold
 vif_dir = '/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue/analysis/fmri/nilearn/singletrial_rampupplateau/vif_summary'
 
 # identify sub directories
-sub_folders = next(os.walk(vif_dir))[1]
-sub_list = [i for i in sorted(sub_folders) if i.startswith('sub-')]
 summary_df = pd.DataFrame()
 threshold = 2.5
-
-for sub in sub_list:
+sub_list = glob.glob(join(vif_dir, f"vif_sub-*.tsv"))
+for sub_fname in sorted(sub_list):
     # load dataframe
-    df = pd.read_csv(join(vif_dir, f"vif_{sub}.tsv"), sep='\t')
+    print(sub_fname)
+    df = pd.read_csv(sub_fname, sep='\t')
+    print(df.head())
     filtered_df = df[df['vif'] > threshold]
     # append metadata with vif greater than threshold
     summary_df = pd.concat([summary_df, filtered_df], ignore_index=True)
-filename = f"singletrial_vif-above-{threshold}.tsv".replace('.', '-')
-summary_df.to_csv(join(vif_dir, filename), sep='\t', index=False)
+filename = f"singletrial_vif-above-{threshold}".replace('.', '-')
+summary_df.to_csv(join(vif_dir, f"{filename}.tsv"), sep='\t', index=False)
 
 
