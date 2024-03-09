02d37140f085411fc725d64cf3480bad40ef3a29
jungheejung <heejung.jung@colorado.edu>
2024-03-09 16:21:33 -0500

    DOC: slurm parameters

--- scripts/step10_nilearn/singletrialLSS/step01_nilearnLSS_rampuppleateau_SUBMIT.sh
+++ scripts/step10_nilearn/singletrialLSS/step01_nilearnLSS_rampuppleateau_SUBMIT.sh
@@ -8,10 +8,10 @@
 #SBATCH -e ./log_ttl/GLM_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-13%10
+#SBATCH --array=1-133%30
 
 conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
 ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR='/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue'
-${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step01_nilearnLSS_rampuppleateau.py --slurm-id ${ID} --session-num 1
+${MAINDIR}/scripts/step10_nilearn/singletrialLSS/step01_nilearnLSS_rampuppleateau.py --slurm-id ${ID} --session-num 4
