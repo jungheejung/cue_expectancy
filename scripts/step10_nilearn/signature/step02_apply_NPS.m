a8e330b2a1411930b374ce826df7ac09ad8e78bf
jungheejung <heejung.jung@colorado.edu>
2024-04-29 22:10:14 -0400

    DEV: apply NPS and extract bold from roi pain pathway

--- scripts/step10_nilearn/signature/step02_apply_NPS.m
+++ scripts/step10_nilearn/signature/step02_apply_NPS.m
@@ -1,77 +1,91 @@
 function step02_apply_NPS()
-    %% Data directories and parameters setup
+%% Data directories and parameters setup
 %     main_dir = '/Volumes/seagate/TMP';
-main_dir = '/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/deriv01_signature/rampup_plateau_brainmask';
-    singletrial_dir = fullfile('/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/singletrial_rampupplateau');
-    sub_list = getSubFolders(singletrial_dir);
-    key_list = {'stimulus'}; % Add 'cue' to this list if needed
+main_dir = '/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/deriv01_signature/rampup_plateau_canlab';
+singletrial_dir = fullfile('/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/singletrial_rampupplateau');
+%     sub_list = getSubFolders(singletrial_dir);
+d = dir(singletrial_dir);
+dfolders = d([d(:).isdir]);
+dfolders = dfolders(~ismember({dfolders(:).name},{'.','..','archive','beh','sub-0000'}));
+% Further filter to include only folders that start with "sub-"
+dfolders_remove = dfolders(startsWith({dfolders.name}, 'sub-'));
 
-    %% Initialize a table to store the results
-    resultTable = table();
+key = 'stimulus';
+sub_list = {dfolders_remove.name};
+key_list = {'stimulus'}; % Add 'cue' to this list if needed
 
-    %% Loop through keys and subjects
-    for k = 1:length(key_list)
-        key = key_list{k};
-        
-        for sub_ind = 1:length(sub_list)
-            sub = sub_list{sub_ind};
-            test_files = dir(fullfile(singletrial_dir, sub, sprintf('*_event-%s*.nii.gz', key)));
+%% Initialize a table to store the results
+resultTable = table();
 
-            %% Process each file
-            for file_ind = 1:length(test_files)
-                dataobj = fmri_data(fullfile(test_files(file_ind).folder, test_files(file_ind).name));
-                [nps_values, image_names, ~, npspos_exp_by_region, npsneg_exp_by_region, npspos, npsneg] = apply_nps(dataobj);
+%     %% Loop through keys and subjects
+%     for k = 1:length(key_list)
+%         key = key_list{k};
+%
+for sub_ind = 1:length(sub_list)
+    subtable = table();
+    sub = sub_list{sub_ind};
+    %             test_files = dir(fullfile(singletrial_dir, sub, sprintf('*_event-%s*.nii.gz', key)));
 
-                %% Prepare region names (assumes pos and neg regions are constant across files)
-                if file_ind == 1 && isempty(resultTable)
-                    posnames = {'vermis', 'rIns', 'rV1', 'rThal', 'lIns', 'rdpIns', 'rS2_Op', 'dACC'};
-                    negnames = {'rLOC', 'lLOC', 'rpLOC', 'pgACC', 'lSTS', 'rIPL', 'PCC'};
-                    allRegionNames = [{'singletrial_fname'}, {'nps_value'}, posnames, negnames];
-                end
+    %% Process each file
+    sub = sub;    ses = '*';    run = '*';    runtype = 'pain';    event = 'stimulus';
+    fname_template = fullfile(singletrial_dir, sub, ...
+        strcat(sub, '_', ses, '_', run, '_runtype-', runtype, '_event-', event,'*.nii.gz'));
+    fname_list = dir(fname_template);
+    flist4table = {fname_list.name};
+    output_table = cell2table(flist4table', 'VariableNames', {'singletrial_fname'});
 
+    dataobj = fmri_data(filenames(fname_template));
+    [nps_values, image_names, ~, npspos_exp_by_region, npsneg_exp_by_region, npspos, npsneg] = apply_nps(dataobj);
 
-
-                allRegionNames = [{'image_name', 'nps_value'}, posnames, negnames];
-                
-                % Ensure the data row matches in number of elements
-                dataRow = [image_names(1), nps_values, npspos_exp_by_region{:}, npsneg_exp_by_region{:}]; % Adjust based on actual data structure
-                dataRowFlat = horzcat(dataRow{1}, dataRow{2}, num2cell(dataRow{3}), num2cell(dataRow{4}));
-                % Then, create the table row
-                if numel(dataRowFlat) == numel(allRegionNames)
-                    newRow = array2table(dataRowFlat, "VariableNames", allRegionNames);
-                else
-                    error('Mismatch between data and variable names lengths');
-                end
-
-                %% Convert nps values and regional expressions to a row in the table
-%                 newRow = array2table([image_names{1}, nps_values{1}, npspos_exp_by_region{:}, npsneg_exp_by_region{:}], "VariableNames", allRegionNames);
-                
-                %% Append to the result table
-                resultTable = [resultTable; newRow];
-            end
-        end
+    %% Prepare region names (assumes pos and neg regions are constant across files)
+    if file_ind == 1 && isempty(resultTable)
+        posnames = {'vermis', 'rIns', 'rV1', 'rThal', 'lIns', 'rdpIns', 'rS2_Op', 'dACC'};
+        negnames = {'rLOC', 'lLOC', 'rpLOC', 'pgACC', 'lSTS', 'rIPL', 'PCC'};
+        allRegionNames = [{'singletrial_fname','NPS','NPSpos', 'NPSneg'} posnames, negnames];
     end
 
-    %% Save the result table to a file (optional)
-    writetable(resultTable, fullfile(main_dir, 'CANlab_applyNPS_singletrial_rampupplateau.csv'));
+    allRegionNames = [{'singletrial_fname', 'NPS','NPSpos', 'NPSneg'}, posnames, negnames];
+
+    % Ensure the data row matches in number of elements
+    dataRow = [image_names{:}, nps_values{:},num2cell(npspos.dat), num2cell(npsneg.dat), npspos_exp_by_region{:}, npsneg_exp_by_region{:}]; % Adjust based on actual data structure
+    dataRowFlat = horzcat(dataRow{1}, dataRow{2}, dataRow{3},dataRow{4},num2cell(dataRow{5}), num2cell(dataRow{6}));
+    cell2table(image_names{:},nps_values{:},npspos.dat, npsneg.dat, npspos_exp_by_region{:}, npsneg_exp_by_region{:})
+    % Then, create the table row
+    if numel(dataRowFlat) == numel(allRegionNames)
+        newRow = array2table(dataRowFlat, "VariableNames", allRegionNames);
+    else
+        error('Mismatch between data and variable names lengths');
+    end
+
+    %% Append to the result table
+    subtable = newRow;
+%     resultTable = [subtable; newRow];
+    resultTable = [subtable; newRow];
+
+writetable(subtable, fullfile(main_dir, strcat(sub,'_CANlab_applyNPS_singletrial_rampupplateau.csv')));
+end
+%     end
+
+%% Save the result table to a file (optional)
+writetable(resultTable, fullfile(main_dir, 'CANlab_applyNPS_singletrial_rampupplateau.csv'));
 end
 
 function subFolders = getSubFolders(singletrial_dir)
-    d = dir(singletrial_dir);
-    isSub = startsWith({d.name}, 'sub-') & [d.isdir];
-    subFolders = {d(isSub).name};
+d = dir(singletrial_dir);
+isSub = startsWith({d.name}, 'sub-') & [d.isdir];
+subFolders = {d(isSub).name};
 end
 
 
 
 % function step02_apply_NPS()
-%     % TODO: 
+%     % TODO:
 %     %% 1. Data directories and parameters
 %     current_dir = pwd;
 %     main_dir = fileparts(fileparts(current_dir));
 %     % sub-0060_ses-04_run-05_runtype-pain_event-stimulus_trial-000_cuetype-low_stimintensity-high.nii.gz
 %     % sub-0060_ses-04_run-06_runtype-cognitive_event-stimulus_trial-000_cuetype-low_stimintensity-high.nii.gz
-%     
+%
 %     %% 2. test run
 %     % main_dir = '/Volumes/spacetop_projects_social';
 % %     singletrial_dir = fullfile(main_dir, 'analysis', 'fmri', 'nilearn', 'singletrial');
@@ -86,9 +100,9 @@ end
 %     key_list = {'stimulus'};%{'cue', 'stimulus'};
 %     % sub = char(sub_list(input));
 %     ses = '*';    run = '*';    runtype = '*';    event = 'stimulus';
-% 
+%
 %     % for s = 1:length(sub_list)
-%     %     sub = char(sub_list(s)); 
+%     %     sub = char(sub_list(s));
 %     for k = 1:length(key_list)
 %         for sub_ind = 1:length(sub_list)
 %         key = char(key_list(k));
@@ -97,9 +111,9 @@ end
 %         % glob all files
 %         test_files = dir(fullfile(singletrial_dir, sub_list{sub_ind}, ...
 %             strcat(sub_list{sub_ind}, '_', ses, '_', run, '_runtype-', runtype, '_event-', key,'*.nii')));
-% 
+%
 %         for file_ind = 1:length(test_files)
-%             
+%
 %             flist4table = {test_files(file_ind).name};
 %             disp(flist4table)
 %     %         output_table = cell2table(flist4table', "VariableNames",  "singletrial_fname");
--- scripts/step10_nilearn/signature/step02_extract_ROI.m
+++ scripts/step10_nilearn/signature/step02_extract_ROI.m
@@ -6,32 +6,53 @@ function step02_extract_ROI()
     % Purpose of this code: to apply NPS to the extracted singletrials.
     %% 1. load filenames as fmri_data
     current_dir = pwd;
-    main_dir = fileparts(fileparts(fileparts(current_dir)));
-    singletrial_dir = fullfile(main_dir, 'analysis', 'fmri', 'nilearn', 'singletrial');
-    nps_dir = fullfile(main_dir, 'analysis', 'fmri', 'nilearn', 'signature_canlabcore');
+%     main_dir = fileparts(fileparts(fileparts(current_dir)));
+    nps_dir = '/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/deriv01_signature/rampup_plateau_painpathway';
+    singletrial_dir = fullfile('/Volumes/spacetop_projects_cue/analysis/fmri/nilearn/singletrial_rampupplateau');
+%     singletrial_dir = fullfile(main_dir, 'analysis', 'fmri', 'nilearn', 'singletrial');
+%     nps_dir = fullfile(main_dir, 'analysis', 'fmri', 'nilearn', 'signature_canlabcore');
     d = dir(singletrial_dir);
     dfolders = d([d(:).isdir]);
-    dfolders_remove = dfolders(~ismember({dfolders(:).name},{'.','..','sub-0000','sub-0002'}));
+    dfolders = dfolders(~ismember({dfolders(:).name},{'.','..','archive','beh','sub-0000'}));
+    % Further filter to include only folders that start with "sub-"
+    dfolders_remove = dfolders(startsWith({dfolders.name}, 'sub-'));
+
+    key = 'stimulus';
     sub_list = {dfolders_remove.name};
-    sub = '*';    ses = '*';    run = '*';    runtype = '*';    event = 'stimulus';
+    for sub_ind = 1:length(sub_list)
+        disp(sub_ind)
+            sub = sub_list{sub_ind};
+            test_files = dir(fullfile(singletrial_dir, sub, sprintf('*_event-%s*.nii.gz', key)));
+
+    sub = sub;    ses = '*';    run = '*';    runtype = 'pain';    event = 'stimulus';
     fname_template = fullfile(singletrial_dir, sub, ...
     strcat(sub, '_', ses, '_', run, '_runtype-', runtype, '_event-', event,'*.nii.gz'));
-    fname_list = dir(fname_template) 
+    fname_list = dir(fname_template);
     flist4table = {fname_list.name};
-    output_table = cell2table(flist4table', "VariableNames",  ["singletrial_fname"]);
+    output_table = cell2table(flist4table', 'VariableNames', {'singletrial_fname'});
 
     dat = fmri_data(filenames(fname_template));
     %% 2. load pain pathwy object
     % pathway_obj = ''
     pain_pathways = load_atlas(which('pain_pathways_atlas_obj.mat'));
-    
-    pain_pathways = pain_pathways.select_atlas_subset({'Thal_VPLM_R','Thal_VPLM_L','Thal_IL','Thal_MD',...
-    'Hythal','pbn_R','pbn_L','Bstem_PAG','rvm_R','Amy_R','Amy_L','dpIns_L','dpIns_R','S2_L','S2_R','mIns_L','mIns_R',...
-    'aIns_L','aIns_R','aMCC_MPFC','s1_foot_L','s1_foot_R','s1_handplus_L','s1_handplus_R'});
-    pain_pathways_mean = extract_roi_averages(dat, fmri_data(pain_pathways), 'unique_mask_values', 'nonorm');
-    % output_table
-    output_table = [ table(pain_pathways_mean.dat, 'VariableNames', pain_pathways.labels)  output_table]; 
+    % full areas
+%     pain_pathways = pain_pathways.select_atlas_subset({'Thal_VPLM_R','Thal_VPLM_L','Thal_IL','Thal_MD',...
+%     'Hythal','pbn_R','pbn_L','Bstem_PAG','rvm_R','Amy_R','Amy_L','dpIns_L','dpIns_R','S2_L','S2_R','mIns_L','mIns_R',...
+%     'aIns_L','aIns_R','aMCC_MPFC','s1_foot_L','s1_foot_R','s1_handplus_L','s1_handplus_R'});
+%     pain_pathways_mean = extract_roi_averages(dat, fmri_data(pain_pathways), 'unique_mask_values', 'nonorm');
+%     % output_table
+%     output_table = [ table(pain_pathways_mean.dat, 'VariableNames', pain_pathways.labels)  output_table]; 
+%     table_fname = fullfile(nps_dir, strcat(sub,'_roi-painpathway_runtype-pain_event-', event, '.csv'));
+%     writetable(output_table, table_fname, 'Delimiter',',');
 
 
-    table_fname = fullfile(nps_dir, strcat('roi-painpathway_sub-all_runtype-pvc_event-', event, '.tsv'));
-    writetable(output_table, table_fname, 'Delimiter',' ');
+
+    pain_pathways_subset = pain_pathways.select_atlas_subset({'dpIns', 'aMCC_MPFC', 'Thal_VPLM','Thal_MD', });
+    pain_pathways_subset_mean = extract_roi_averages(dat, fmri_data(pain_pathways_subset), 'unique_mask_values', 'nonorm');
+    output_table = [ table(pain_pathways_subset_mean.dat, 'VariableNames', pain_pathways_subset.labels)  output_table]; 
+    table_fname = fullfile(nps_dir, strcat(sub,'_roi-painpathwaysubset_runtype-pain_event-', event, '.csv'));
+    writetable(output_table, table_fname, 'Delimiter',',');
+    end
+%     table_fname = fullfile(nps_dir, strcat('roi-painpathway_sub-all_runtype-pvc_event-', event, '.tsv'));
+%     writetable(output_table, table_fname, 'Delimiter',' ');
+end
