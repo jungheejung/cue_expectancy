610597e97e0c8bf752ff62c9db77474500ef2d1b
jungheejung <heejung.jung@colorado.edu>
2024-09-03 15:21:09 -0400

    DEV: extract metadata from single trial fname

--- scripts/utils/bids_utils.py
+++ scripts/utils/bids_utils.py
@@ -64,3 +64,50 @@ def filter_files(fmriprep_files, behavioral_files, bad_json_fname):
                 break
 
     return filtered_fmriprep, filtered_behavioral
+
+
+def extract_metadata_singletrial(filename):
+    # Regular expression pattern to match the filename structure
+    pattern = (r"(?P<sub>sub-\d+)_"
+               r"(?P<ses>ses-\d+)_"
+               r"(?P<run>run-\d+)_"
+               r"runtype-(?P<runtype>[a-zA-Z]+)_"
+               r"event-(?P<event>[a-zA-Z]+)_"
+               r"trial-(?P<trial>\d+)_"
+               r"cuetype-(?P<cuetype>[a-zA-Z]+)"
+               r"(?:_stimintensity-(?P<stimintensity>[a-zA-Z]+))?")  # Optional stimintensity
+
+    match = re.search(pattern, filename)
+    
+    if match:
+        metadata = match.groupdict()
+        metadata['trial_int'] = int(metadata['trial'])
+        metadata['trial_index'] = int(metadata['trial']) + 1
+        return metadata
+    else:
+        return None
+    
+
+def get_unique_sub_ids(directory):
+    """
+    Extracts and returns a sorted list of unique 'sub-0000' IDs from filenames in a given directory.
+
+    Parameters:
+    directory (str): The path to the directory containing the files.
+
+    Returns:
+    list: A sorted list of unique 'sub-0000' IDs.
+    """
+    unique_sub_ids = set()
+    pattern = r"(sub-\d{4})"
+    
+    # Loop through all files in the directory
+    for filename in os.listdir(directory):
+        # Only process files that match the pattern
+        match = re.search(pattern, filename)
+        if match:
+            unique_sub_ids.add(match.group(1))
+
+    # Convert the set to a sorted list
+    unique_sub_ids_list = sorted(unique_sub_ids)
+    return unique_sub_ids_list
