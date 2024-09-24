458a53c2a8603e92c4894b510823aa63b44b52b4
jungheejung <heejung.jung@colorado.edu>
2024-09-23 21:35:24 -0400

    BUG: update filepath - wrapper script not updated

--- scripts/step10_nilearn/covariates/step01_singletrial_SLURM_PE.sh
+++ scripts/step10_nilearn/covariates/step01_singletrial_SLURM_PE.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=12gb
 #SBATCH --time=00:10:00
-#SBATCH -o ./log_PE/PE_%A_%a.o
-#SBATCH -e ./log_PE/PE_%A_%a.e
+#SBATCH -o ./log_PE_c/PE_%A_%a.o
+#SBATCH -e ./log_PE_c/PE_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
 #SBATCH --array=1-133%100
@@ -16,9 +16,10 @@ ID=$((SLURM_ARRAY_TASK_ID-1))
 MAINDIR="/dartfs-hpc/rc/lab/C/CANlab/labdata/projects/spacetop_projects_cue"
 SAVEDIR="${MAINDIR}/analysis/fmri/nilearn/deriv04_covariate/"
 CANLABCORE="/dartfs-hpc/rc/lab/C/CANlab/modules/CanlabCore"
-python ${MAINDIR}/scripts/step10_nilearn/covariates/singletrial_covPE.py \
+# task options: 'pain','vicarious','cognitive'
+python ${MAINDIR}/scripts/step10_nilearn/covariates/step01_singletrial_covPE.py \
 --slurm-id ${ID} \
---tasktype "pain" \
+--tasktype "cognitive" \
 --fmri-event "stimulus" \
 --beh-regressor "PE_mdl2" \
 --beh-savename "cov_PE" \
